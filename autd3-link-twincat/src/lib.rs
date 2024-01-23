2583c33f1a114572db3e6da9de3226e3b8988e24
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-01-23 16:25:31 +0900

    rm header from link-twincat

--- autd3-link-twincat/src/lib.rs
+++ autd3-link-twincat/src/lib.rs
@@ -1,16 +1,3 @@
-/*
- * File: lib.rs
- * Project: src
- * Created Date: 27/05/2021
- * Author: Shun Suzuki
- * -----
- * Last Modified: 26/05/2023
- * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
- * -----
- * Copyright (c) 2021 Shun Suzuki. All rights reserved.
- *
- */
-
 mod error;
 
 #[cfg(feature = "local")]
