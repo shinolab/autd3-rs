ac2b0beb0e33e5be0133a42a5358b8105f4bb299
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-01-23 16:23:11 +0900

    make backend type Arc

--- autd3-gain-holo/src/linear_synthesis/gspat.rs
+++ autd3-gain-holo/src/linear_synthesis/gspat.rs
@@ -1,17 +1,4 @@
-/*
- * File: gspat.rs
- * Project: linear_synthesis
- * Created Date: 29/05/2021
- * Author: Shun Suzuki
- * -----
- * Last Modified: 19/01/2024
- * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
- * -----
- * Copyright (c) 2021 Shun Suzuki. All rights reserved.
- *
- */
-
-use std::{collections::HashMap, rc::Rc};
+use std::{collections::HashMap, sync::Arc};
 
 use crate::{
     constraint::EmissionConstraint, helper::generate_result, impl_holo, Amplitude, Complex,
@@ -33,13 +20,13 @@ pub struct GSPAT<B: LinAlgBackend + 'static> {
     amps: Vec<Amplitude>,
     repeat: usize,
     constraint: EmissionConstraint,
-    backend: Rc<B>,
+    backend: Arc<B>,
 }
 
 impl_holo!(B, GSPAT<B>);
 
 impl<B: LinAlgBackend + 'static> GSPAT<B> {
-    pub const fn new(backend: Rc<B>) -> Self {
+    pub const fn new(backend: Arc<B>) -> Self {
         Self {
             foci: vec![],
             amps: vec![],
--- autd3-gain-holo/src/matrix/sdp.rs
+++ autd3-gain-holo/src/matrix/sdp.rs
@@ -1,17 +1,4 @@
-/*
- * File: sdp.rs
- * Project: matrix
- * Created Date: 28/05/2021
- * Author: Shun Suzuki
- * -----
- * Last Modified: 19/01/2024
- * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
- * -----
- * Copyright (c) 2021 Shun Suzuki. All rights reserved.
- *
- */
-
-use std::{collections::HashMap, rc::Rc};
+use std::{collections::HashMap, sync::Arc};
 
 use rand::Rng;
 
@@ -37,13 +24,13 @@ pub struct SDP<B: LinAlgBackend + 'static> {
     lambda: float,
     repeat: usize,
     constraint: EmissionConstraint,
-    backend: Rc<B>,
+    backend: Arc<B>,
 }
 
 impl_holo!(B, SDP<B>);
 
 impl<B: LinAlgBackend + 'static> SDP<B> {
-    pub const fn new(backend: Rc<B>) -> Self {
+    pub const fn new(backend: Arc<B>) -> Self {
         Self {
             foci: vec![],
             amps: vec![],
