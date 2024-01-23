f9f847044b756224a9cd4078232875c38b2c43b1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-01-23 16:26:16 +0900

    rm header from modulation-audio-file

--- autd3-modulation-audio-file/src/rawpcm.rs
+++ autd3-modulation-audio-file/src/rawpcm.rs
@@ -1,16 +1,3 @@
-/*
- * File: rawpcm.rs
- * Project: src
- * Created Date: 15/06/2023
- * Author: Shun Suzuki
- * -----
- * Last Modified: 19/01/2024
- * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
- * -----
- * Copyright (c) 2023 Shun Suzuki. All rights reserved.
- *
- */
-
 use autd3_driver::{common::EmitIntensity, derive::*};
 
 use std::{
@@ -55,7 +42,6 @@ impl RawPCM {
 }
 
 impl Modulation for RawPCM {
-    #[allow(clippy::unnecessary_cast)]
     fn calc(&self) -> Result<Vec<EmitIntensity>, AUTDInternalError> {
         Ok(wav_io::resample::linear(
             self.raw_buffer.clone(),
@@ -66,5 +52,51 @@ impl Modulation for RawPCM {
         .iter()
         .map(|&d| EmitIntensity::new((d * 255.).round() as u8))
         .collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+
+    fn create_dat<P: AsRef<Path>>(path: P, data: &[u8]) {
+        std::fs::create_dir_all(path.as_ref().parent().unwrap()).unwrap();
+        if path.as_ref().exists() {
+            std::fs::remove_file(path.as_ref()).unwrap();
+        }
+        let mut f = File::create(path).unwrap();
+        f.write_all(data).unwrap();
+    }
+
+    #[test]
+    fn test_rawpcm_new() {
+        let home_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let path = Path::new(&home_dir).join("tmp").join("tmp.dat");
+        create_dat(&path, &[0xFF, 0x7F, 0x00]);
+        let m = RawPCM::new(&path, 4000);
+        assert!(m.is_ok());
+        let m = m.unwrap();
+        assert_eq!(
+            m.calc().unwrap(),
+            vec![
+                EmitIntensity::new(0xFF),
+                EmitIntensity::new(0x7F),
+                EmitIntensity::new(0x00)
+            ]
+        );
+
+        let m = RawPCM::new("not_exists.dat", 4000);
+        assert!(m.is_err());
+    }
+
+    #[test]
+    fn test_rawpcm_clone() {
+        let home_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let path = Path::new(&home_dir).join("tmp").join("tmp2.dat");
+        create_dat(&path, &[0xFF, 0xFF]);
+        let m = RawPCM::new(Path::new(&home_dir).join("tmp").join("tmp2.dat"), 4000).unwrap();
+        let m2 = m.clone();
+        assert_eq!(m.sampling_config(), m2.sampling_config());
     }
 }
