f3d7d33b0703306a4e8326a74c99bb0d359b8a9a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-01-28 12:31:07 +0900

    update lightweight-server example

--- examples/src/lightweight_server.rs
+++ examples/src/lightweight_server.rs
@@ -1,30 +1,33 @@
-use autd3_protobuf::lightweight::LightweightServer;
-use autd3_protobuf::*;
+use autd3_link_soem::{Status, SOEM};
+use autd3_protobuf::{lightweight::LightweightServer, *};
 
-use tokio::{runtime::Runtime, sync::mpsc};
+use tokio::sync::mpsc;
 use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let server = LightweightServer::new(|| -> autd3_link_soem::local::link_soem::SOEMBuilder {
-        autd3_link_soem::SOEM::builder()
+    let server = LightweightServer::new(|| {
+        SOEM::builder()
             .with_timeout(std::time::Duration::from_millis(200))
-            .with_on_err(|msg| {
-                eprintln!("{}", msg);
-            })
-            .with_on_lost(|msg| {
-                eprintln!("{}", msg);
-                std::process::exit(-1);
+            .with_err_handler(|slave, status| match status {
+                Status::Error(msg) => eprintln!("Error [{}]: {}", slave, msg),
+                Status::Lost(msg) => {
+                    eprintln!("Lost [{}]: {}", slave, msg);
+                    std::process::exit(-1);
+                }
+                Status::StateChanged(msg) => eprintln!("StateChanged [{}]: {}", slave, msg),
             })
     });
 
     let (tx, mut rx) = mpsc::channel(1);
-    ctrlc::set_handler(move || {
-        let rt = Runtime::new().expect("failed to obtain a new Runtime object");
-        rt.block_on(tx.send(())).unwrap();
+    ctrlc_async::set_async_handler(async move {
+        let _ = tx.send(()).await;
     })
     .expect("Error setting Ctrl-C handler");
 
+    println!("Starting server...");
+    println!("Wainting client to connect...");
+    println!("Press Ctrl-C to shutdown the server.");
     Server::builder()
         .add_service(ecat_light_server::EcatLightServer::new(server))
         .serve_with_shutdown(format!("0.0.0.0:{}", 8080).parse()?, async {
