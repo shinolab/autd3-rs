0b5c7faa577713d8b72a4e465901a20c038e6732
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-01-31 11:23:02 +0900

    refactor autd3 test

--- autd3/tests/link/nop.rs
+++ autd3/tests/link/nop.rs
@@ -1,25 +1,26 @@
 use autd3::prelude::*;
 
 #[tokio::test]
-async fn nop_test() {
+async fn nop_test() -> anyhow::Result<()> {
     let mut autd = Controller::builder()
         .add_device(AUTD3::new(Vector3::zeros()))
         .open_with(Nop::builder().with_timeout(std::time::Duration::from_millis(100)))
-        .await
-        .unwrap();
+        .await?;
 
-    assert_eq!(autd.link.timeout(), std::time::Duration::from_millis(100));
+    assert_eq!(std::time::Duration::from_millis(100), autd.link.timeout());
 
-    assert_eq!(autd.send(Static::new()).await, Ok(true));
+    assert!(autd.send(Static::new()).await?);
 
-    assert_eq!(autd.close().await, Ok(true));
+    assert!(autd.close().await?);
 
     assert_eq!(
-        autd.send(Static::new()).await,
-        Err(AUTDError::Internal(AUTDInternalError::LinkClosed))
+        Err(AUTDError::Internal(AUTDInternalError::LinkClosed)),
+        autd.send(Static::new()).await
     );
     assert_eq!(
-        autd.fpga_state().await,
-        Err(AUTDError::Internal(AUTDInternalError::LinkClosed))
+        Err(AUTDError::Internal(AUTDInternalError::LinkClosed)),
+        autd.fpga_state().await
     );
+
+    Ok(())
 }
