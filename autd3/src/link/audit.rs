e9d3517be92a6f0e4abfe158f8ad3dbb252d0f91
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-01-31 15:25:26 +0900

    add trace to Link trait

--- autd3/src/link/audit.rs
+++ autd3/src/link/audit.rs
@@ -14,6 +14,7 @@ use autd3_firmware_emulator::CPUEmulator;
 pub struct Audit {
     is_open: bool,
     timeout: Duration,
+    last_timeout: Option<Duration>,
     cpus: Vec<CPUEmulator>,
     down: bool,
     broken: bool,
@@ -41,6 +42,7 @@ impl LinkBuilder for AuditBuilder {
         Ok(Audit {
             is_open: true,
             timeout: self.timeout,
+            last_timeout: None,
             cpus: geometry
                 .iter()
                 .enumerate()
@@ -57,6 +59,10 @@ impl Audit {
         AuditBuilder {
             timeout: Duration::ZERO,
         }
+    }
+
+    pub const fn last_timeout(&self) -> Option<Duration> {
+        self.last_timeout
     }
 
     pub fn emulators(&self) -> &[CPUEmulator] {
@@ -152,5 +158,9 @@ impl Link for Audit {
 
     fn timeout(&self) -> Duration {
         self.timeout
+    }
+
+    fn trace(&mut self, _: &TxDatagram, _: &mut [RxMessage], timeout: Option<Duration>) {
+        self.last_timeout = timeout;
     }
 }
