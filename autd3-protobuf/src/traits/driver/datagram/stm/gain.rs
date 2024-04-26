32028f3c3afc972f8b1e1e2542687dc0881bd233
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-04-26 15:36:35 +0900

    update protobuf

--- autd3-protobuf/src/traits/driver/datagram/stm/gain.rs
+++ autd3-protobuf/src/traits/driver/datagram/stm/gain.rs
@@ -61,60 +61,61 @@ impl FromMessage<GainStm>
 {
     #[allow(clippy::unnecessary_cast)]
     fn from_msg(msg: &GainStm) -> Option<Self> {
-        autd3_driver::datagram::GainSTM::from_sampling_config(
-            SamplingConfiguration::from_division_raw(msg.freq_div).ok()?,
+        Some(
+            autd3_driver::datagram::GainSTM::from_sampling_config(
+                SamplingConfiguration::from_division_raw(msg.freq_div).ok()?,
+            )
+            .with_loop_behavior(autd3_driver::firmware::fpga::LoopBehavior::from_msg(
+                msg.loop_behavior.as_ref()?,
+            )?)
+            .add_gains_from_iter(msg.gains.iter().filter_map(|gain| match &gain.gain {
+                Some(gain::Gain::Focus(msg)) => autd3::prelude::Focus::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Bessel(msg)) => autd3::prelude::Bessel::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Null(msg)) => autd3::prelude::Null::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Plane(msg)) => autd3::prelude::Plane::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Uniform(msg)) => autd3::prelude::Uniform::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Sdp(msg)) => autd3_gain_holo::SDP::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Naive(msg)) => autd3_gain_holo::Naive::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Gs(msg)) => autd3_gain_holo::GS::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Gspat(msg)) => autd3_gain_holo::GSPAT::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Lm(msg)) => autd3_gain_holo::LM::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                Some(gain::Gain::Greedy(msg)) => autd3_gain_holo::Greedy::from_msg(msg).map(|g| {
+                    let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
+                    g
+                }),
+                None => None,
+            })),
         )
-        .with_loop_behavior(autd3_driver::firmware::fpga::LoopBehavior::from_msg(
-            msg.loop_behavior.as_ref()?,
-        )?)
-        .add_gains_from_iter(msg.gains.iter().filter_map(|gain| match &gain.gain {
-            Some(gain::Gain::Focus(msg)) => autd3::prelude::Focus::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Bessel(msg)) => autd3::prelude::Bessel::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Null(msg)) => autd3::prelude::Null::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Plane(msg)) => autd3::prelude::Plane::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Uniform(msg)) => autd3::prelude::Uniform::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Sdp(msg)) => autd3_gain_holo::SDP::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Naive(msg)) => autd3_gain_holo::Naive::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Gs(msg)) => autd3_gain_holo::GS::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Gspat(msg)) => autd3_gain_holo::GSPAT::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Lm(msg)) => autd3_gain_holo::LM::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            Some(gain::Gain::Greedy(msg)) => autd3_gain_holo::Greedy::from_msg(msg).map(|g| {
-                let g: Box<dyn autd3_driver::datagram::Gain + Send + 'static> = Box::new(g);
-                g
-            }),
-            None => None,
-        }))
-        .ok()
     }
 }
 
