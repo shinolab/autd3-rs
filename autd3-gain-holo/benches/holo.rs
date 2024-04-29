451d585688ae95ea30917e366d966452b1008d92
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-04-30 08:59:38 +0900

    update gain-holo bench

--- autd3-gain-holo/benches/holo.rs
+++ autd3-gain-holo/benches/holo.rs
@@ -1,8 +1,18 @@
 #[cfg(feature = "bench-utilities")]
 criterion::criterion_group!(
     benches,
-    autd3_gain_holo::bench_utilities::foci::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::devices::<autd3_gain_holo::NalgebraBackend, 2>
+    autd3_gain_holo::bench_utilities::sdp_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::naive_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::gs_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::gspat_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::lm_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::greedy_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::sdp_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::naive_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::gs_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::gspat_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::lm_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
+    autd3_gain_holo::bench_utilities::greedy_over_devices::<autd3_gain_holo::NalgebraBackend, 4>
 );
 #[cfg(feature = "bench-utilities")]
 criterion::criterion_main!(benches);
