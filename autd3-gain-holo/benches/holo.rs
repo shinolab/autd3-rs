cef0e5a5bb1376f0654a7c76bdb71cfe33f85871
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-04-30 09:26:49 +0900

    rm `parallel` feature

--- autd3-gain-holo/benches/holo.rs
+++ autd3-gain-holo/benches/holo.rs
@@ -1,18 +1,54 @@
 #[cfg(feature = "bench-utilities")]
 criterion::criterion_group!(
     benches,
-    autd3_gain_holo::bench_utilities::sdp_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::naive_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::gs_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::gspat_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::lm_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::greedy_over_foci::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::sdp_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::naive_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::gs_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::gspat_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::lm_over_devices::<autd3_gain_holo::NalgebraBackend, 4>,
-    autd3_gain_holo::bench_utilities::greedy_over_devices::<autd3_gain_holo::NalgebraBackend, 4>
+    autd3_gain_holo::bench_utilities::sdp_over_foci::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::naive_over_foci::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::gs_over_foci::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::gspat_over_foci::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::lm_over_foci::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::greedy_over_foci::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::sdp_over_devices::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::naive_over_devices::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::gs_over_devices::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::gspat_over_devices::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::lm_over_devices::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >,
+    autd3_gain_holo::bench_utilities::greedy_over_devices::<
+        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
+    >
 );
 #[cfg(feature = "bench-utilities")]
 criterion::criterion_main!(benches);
