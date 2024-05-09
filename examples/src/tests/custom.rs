8ec764bcec3cc266de9bb87448fa854e11f71226
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-09 11:12:11 +0900

    add `modulation::Custom`

--- examples/src/tests/custom.rs
+++ examples/src/tests/custom.rs
@@ -3,8 +3,11 @@ use autd3::prelude::*;
 pub async fn custom(autd: &mut Controller<impl Link>) -> anyhow::Result<bool> {
     autd.send(ConfigureSilencer::disable()).await?;
 
-    let m = Static::new();
-    let g = Custom::new(|dev| {
+    let m = autd3::modulation::Custom::new(SamplingConfig::DISABLE, |dev| match dev.idx() {
+        0 => Ok(vec![0xFF, 0xFF]),
+        _ => Ok(vec![0x00, 0x00]),
+    });
+    let g = autd3::gain::Custom::new(|dev| {
         let dev_idx = dev.idx();
         move |tr| match (dev_idx, tr.idx()) {
             (0, 0) => Drive::new(Phase::new(0), EmitIntensity::new(0xFF)),
