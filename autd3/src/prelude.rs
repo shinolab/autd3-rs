7260fcf1cc7b67305eedd64bb869acca66dc324a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-11 11:50:48 +0900

    add `rad` and `deg` to `autd3::prelude`

--- autd3/src/prelude.rs
+++ autd3/src/prelude.rs
@@ -8,17 +8,15 @@ pub use crate::{
 
 pub use autd3_driver::{
     autd3_device::AUTD3,
-    datagram::ControlPoint,
     datagram::{
-        Clear, DebugSettings, EmulateGPIOIn, FocusSTM, ForceFan, GainCache, GainFilter, GainSTM,
-        GainTransform, Group, IntoDatagramWithSegment, IntoDatagramWithSegmentTransition,
-        IntoDatagramWithTimeout, IntoGainCache, IntoGainTransform, IntoModulationCache,
-        IntoModulationTransform, IntoRadiationPressure, Modulation, ModulationCache,
-        ModulationProperty, ModulationTransform, PhaseFilter, PulseWidthEncoder, RadiationPressure,
-        ReadsFPGAState, Silencer, SwapSegment, Synchronize,
+        Clear, ControlPoint, DebugSettings, EmulateGPIOIn, FocusSTM, ForceFan, GainCache,
+        GainFilter, GainSTM, GainTransform, Group, IntoDatagramWithSegment,
+        IntoDatagramWithSegmentTransition, IntoDatagramWithTimeout, IntoGainCache,
+        IntoGainTransform, IntoModulationCache, IntoModulationTransform, IntoRadiationPressure,
+        Modulation, ModulationCache, ModulationProperty, ModulationTransform, PhaseFilter,
+        PulseWidthEncoder, RadiationPressure, ReadsFPGAState, Silencer, SwapSegment, Synchronize,
     },
-    defined::{kHz, Hz},
-    defined::{mm, PI},
+    defined::{deg, kHz, mm, rad, Hz, PI},
     error::AUTDInternalError,
     ethercat::DcSysTime,
     firmware::{
