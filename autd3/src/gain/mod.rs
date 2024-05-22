b558a321ad302047ea91caf2243e85f6333c2aff
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-22 09:53:11 +0900

    add `Bessel2`

--- autd3/src/gain/mod.rs
+++ autd3/src/gain/mod.rs
@@ -1,11 +1,14 @@
 mod bessel;
+mod bessel2;
 mod custom;
 mod focus;
 mod null;
 mod plane;
 mod uniform;
 
+#[allow(deprecated)]
 pub use bessel::Bessel;
+pub use bessel2::Bessel2;
 pub use custom::Custom;
 pub use focus::Focus;
 pub use null::Null;
