465deb285a90817f25305d150978aa20752d6f7f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-30 06:48:39 +0900

    improve gain cache perf

--- autd3-driver/benches/gain.rs
+++ autd3-driver/benches/gain.rs
@@ -19,7 +19,7 @@ pub fn generate_geometry(size: usize) -> Geometry {
     )
 }
 
-#[derive(Clone, PartialEq, Debug)]
+#[derive(Gain, Clone, PartialEq, Debug)]
 pub struct Focus {
     pos: Vector3,
     intensity: EmitIntensity,
@@ -50,21 +50,6 @@ impl Gain for Focus {
                 )
             })
         }))
-    }
-}
-
-impl Datagram for Focus {
-    type O1 = GainOp;
-    type O2 = NullOp;
-    type G = GainOperationGenerator;
-
-    fn operation_generator(self, geometry: &Geometry) -> Result<Self::G, AUTDInternalError> {
-        let g = self.calc(geometry)?;
-        Ok(Self::G {
-            g: Box::new(g),
-            segment: Segment::S0,
-            transition: true,
-        })
     }
 }
 
@@ -118,5 +103,31 @@ fn focus_parallel(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, focus, focus_parallel);
+fn focus_cache(c: &mut Criterion) {
+    let mut group = c.benchmark_group("autd3/gain/focus-cache");
+
+    [1, 10].iter().for_each(|&size| {
+        group.bench_with_input(
+            BenchmarkId::new("Gain::FocusCache", size),
+            &generate_geometry(size),
+            |b, geometry| {
+                let mut tx = TxDatagram::new(size);
+                let g = Focus::new(Vector3::new(
+                    black_box(90.),
+                    black_box(70.),
+                    black_box(150.),
+                ))
+                .with_cache();
+                b.iter(|| {
+                    let gen = g.clone().operation_generator(geometry).unwrap();
+                    let mut operations = OperationHandler::generate(gen, geometry);
+                    OperationHandler::pack(&mut operations, geometry, &mut tx, usize::MAX).unwrap();
+                })
+            },
+        );
+    });
+    group.finish();
+}
+
+criterion_group!(benches, focus, focus_parallel, focus_cache);
 criterion_main!(benches);
