a3d43bf9794701d8e8c1eae5d7eecb30d221b72c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-31 06:12:47 +0900

    update holo bench

--- autd3-gain-holo/benches/holo.rs
+++ autd3-gain-holo/benches/holo.rs
@@ -1,54 +1,37 @@
 #[cfg(feature = "bench-utilities")]
 criterion::criterion_group!(
     benches,
-    autd3_gain_holo::bench_utilities::sdp_over_foci::<
+    autd3_gain_holo::bench_utilities::sdp::<
+        autd3_driver::acoustics::directivity::Sphere,
         autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
         4,
     >,
-    autd3_gain_holo::bench_utilities::naive_over_foci::<
+    autd3_gain_holo::bench_utilities::naive::<
+        autd3_driver::acoustics::directivity::Sphere,
         autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
         4,
     >,
-    autd3_gain_holo::bench_utilities::gs_over_foci::<
+    autd3_gain_holo::bench_utilities::gs::<
+        autd3_driver::acoustics::directivity::Sphere,
         autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
         4,
     >,
-    autd3_gain_holo::bench_utilities::gspat_over_foci::<
+    autd3_gain_holo::bench_utilities::gspat::<
+        autd3_driver::acoustics::directivity::Sphere,
         autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
         4,
     >,
-    autd3_gain_holo::bench_utilities::lm_over_foci::<
+    autd3_gain_holo::bench_utilities::lm::<
+        autd3_driver::acoustics::directivity::Sphere,
         autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
+        4,
         4,
     >,
-    autd3_gain_holo::bench_utilities::greedy_over_foci::<
-        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
-        4,
-    >,
-    autd3_gain_holo::bench_utilities::sdp_over_devices::<
-        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
-        4,
-    >,
-    autd3_gain_holo::bench_utilities::naive_over_devices::<
-        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
-        4,
-    >,
-    autd3_gain_holo::bench_utilities::gs_over_devices::<
-        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
-        4,
-    >,
-    autd3_gain_holo::bench_utilities::gspat_over_devices::<
-        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
-        4,
-    >,
-    autd3_gain_holo::bench_utilities::lm_over_devices::<
-        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
-        4,
-    >,
-    autd3_gain_holo::bench_utilities::greedy_over_devices::<
-        autd3_gain_holo::NalgebraBackend<autd3_driver::acoustics::directivity::Sphere>,
-        4,
-    >
+    autd3_gain_holo::bench_utilities::greedy::<autd3_driver::acoustics::directivity::Sphere, 4, 4>,
 );
 #[cfg(feature = "bench-utilities")]
 criterion::criterion_main!(benches);
--- autd3-gain-holo/src/bench_utilities.rs
+++ autd3-gain-holo/src/bench_utilities.rs
@@ -1,7 +1,7 @@
-use criterion::{black_box, AxisScale, BenchmarkId, Criterion, PlotConfiguration};
+use criterion::{black_box, AxisScale, Criterion, PlotConfiguration};
 
 use autd3_driver::{
-    acoustics::directivity::Sphere,
+    acoustics::directivity::Directivity,
     autd3_device::AUTD3,
     defined::{FREQ_40K, PI},
     derive::Datagram,
@@ -35,304 +35,175 @@ pub fn gen_foci(n: usize, num_dev: usize) -> impl IntoIterator<Item = (Vector3, 
     })
 }
 
-pub fn sdp_over_foci<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/sdp_with_{}_devices", N));
+pub fn sdp<
+    D: Directivity + 'static,
+    B: LinAlgBackend<D> + 'static,
+    const NUM_DEV: usize,
+    const NUM_FOCI: usize,
+>(
+    c: &mut Criterion,
+) {
+    let mut group = c.benchmark_group("autd3-gain-holo");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
     let backend = B::new().unwrap();
-    [2].into_iter()
-        .chain((2..6).map(|i| i * i))
-        .for_each(|size| {
-            group.bench_with_input(
-                BenchmarkId::new("with_foci_num", size),
-                &generate_geometry(N),
-                |b, geometry| {
-                    let mut tx = TxDatagram::new(size);
-                    b.iter(|| {
-                        let gen = SDP::new(backend.clone(), gen_foci(size, N))
-                            .operation_generator(geometry)
-                            .unwrap();
-                        let mut operations = OperationHandler::generate(gen, geometry);
-                        OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                    })
-                },
-            );
-        });
+
+    group.bench_with_input(
+        format!("SDP/{}-devices_{}-foci", NUM_DEV, NUM_FOCI),
+        &generate_geometry(NUM_DEV),
+        |b, geometry| {
+            let mut tx = TxDatagram::new(NUM_DEV);
+            b.iter(|| {
+                let gen = SDP::new(backend.clone(), gen_foci(NUM_FOCI, NUM_DEV))
+                    .operation_generator(geometry)
+                    .unwrap();
+                let mut operations = OperationHandler::generate(gen, geometry);
+                OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
+            })
+        },
+    );
     group.finish();
 }
 
-pub fn naive_over_foci<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/naive_with_{}_devices", N));
+pub fn naive<
+    D: Directivity + 'static,
+    B: LinAlgBackend<D> + 'static,
+    const NUM_DEV: usize,
+    const NUM_FOCI: usize,
+>(
+    c: &mut Criterion,
+) {
+    let mut group = c.benchmark_group("autd3-gain-holo");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
     let backend = B::new().unwrap();
-    [2].into_iter()
-        .chain((2..6).map(|i| i * i))
-        .for_each(|size| {
-            group.bench_with_input(
-                BenchmarkId::new("with_foci_num", size),
-                &generate_geometry(N),
-                |b, geometry| {
-                    let mut tx = TxDatagram::new(size);
-                    b.iter(|| {
-                        let gen = Naive::new(backend.clone(), gen_foci(size, N))
-                            .operation_generator(geometry)
-                            .unwrap();
-                        let mut operations = OperationHandler::generate(gen, geometry);
-                        OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                    })
-                },
-            );
-        });
+
+    group.bench_with_input(
+        format!("Naive/{}_devices-{}-foci", NUM_DEV, NUM_FOCI),
+        &generate_geometry(NUM_DEV),
+        |b, geometry| {
+            let mut tx = TxDatagram::new(NUM_DEV);
+            b.iter(|| {
+                let gen = Naive::new(backend.clone(), gen_foci(NUM_FOCI, NUM_DEV))
+                    .operation_generator(geometry)
+                    .unwrap();
+                let mut operations = OperationHandler::generate(gen, geometry);
+                OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
+            })
+        },
+    );
     group.finish();
 }
 
-pub fn gs_over_foci<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/gs_with_{}_devices", N));
+pub fn gs<
+    D: Directivity + 'static,
+    B: LinAlgBackend<D> + 'static,
+    const NUM_DEV: usize,
+    const NUM_FOCI: usize,
+>(
+    c: &mut Criterion,
+) {
+    let mut group = c.benchmark_group("autd3-gain-holo");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
     let backend = B::new().unwrap();
-    [2].into_iter()
-        .chain((2..6).map(|i| i * i))
-        .for_each(|size| {
-            group.bench_with_input(
-                BenchmarkId::new("with_foci_num", size),
-                &generate_geometry(N),
-                |b, geometry| {
-                    let mut tx = TxDatagram::new(size);
-                    b.iter(|| {
-                        let gen = GS::new(backend.clone(), gen_foci(size, N))
-                            .operation_generator(geometry)
-                            .unwrap();
-                        let mut operations = OperationHandler::generate(gen, geometry);
-                        OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                    })
-                },
-            );
-        });
+
+    group.bench_with_input(
+        format!("GS/{}_devices-{}-foci", NUM_DEV, NUM_FOCI),
+        &generate_geometry(NUM_DEV),
+        |b, geometry| {
+            let mut tx = TxDatagram::new(NUM_DEV);
+            b.iter(|| {
+                let gen = GS::new(backend.clone(), gen_foci(NUM_FOCI, NUM_DEV))
+                    .operation_generator(geometry)
+                    .unwrap();
+                let mut operations = OperationHandler::generate(gen, geometry);
+                OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
+            })
+        },
+    );
     group.finish();
 }
 
-pub fn gspat_over_foci<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/gspat_with_{}_devices", N));
+pub fn gspat<
+    D: Directivity + 'static,
+    B: LinAlgBackend<D> + 'static,
+    const NUM_DEV: usize,
+    const NUM_FOCI: usize,
+>(
+    c: &mut Criterion,
+) {
+    let mut group = c.benchmark_group("autd3-gain-holo");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
     let backend = B::new().unwrap();
-    [2].into_iter()
-        .chain((2..6).map(|i| i * i))
-        .for_each(|size| {
-            group.bench_with_input(
-                BenchmarkId::new("with_foci_num", size),
-                &generate_geometry(N),
-                |b, geometry| {
-                    let mut tx = TxDatagram::new(size);
-                    b.iter(|| {
-                        let gen = GSPAT::new(backend.clone(), gen_foci(size, N))
-                            .operation_generator(geometry)
-                            .unwrap();
-                        let mut operations = OperationHandler::generate(gen, geometry);
-                        OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                    })
-                },
-            );
-        });
+
+    group.bench_with_input(
+        format!("GSPAT/{}_devices-{}-foci", NUM_DEV, NUM_FOCI),
+        &generate_geometry(NUM_DEV),
+        |b, geometry| {
+            let mut tx = TxDatagram::new(NUM_DEV);
+            b.iter(|| {
+                let gen = GSPAT::new(backend.clone(), gen_foci(NUM_FOCI, NUM_DEV))
+                    .operation_generator(geometry)
+                    .unwrap();
+                let mut operations = OperationHandler::generate(gen, geometry);
+                OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
+            })
+        },
+    );
     group.finish();
 }
 
-pub fn lm_over_foci<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/lm_with_{}_devices", N));
-    group
-        .sample_size(10)
-        .plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
-
-    let backend = B::new().unwrap();
-    [2].into_iter()
-        .chain((2..6).map(|i| i * i))
-        .for_each(|size| {
-            group.bench_with_input(
-                BenchmarkId::new("with_foci_num", size),
-                &generate_geometry(N),
-                |b, geometry| {
-                    let mut tx = TxDatagram::new(size);
-                    b.iter(|| {
-                        let gen = LM::new(backend.clone(), gen_foci(size, N))
-                            .operation_generator(geometry)
-                            .unwrap();
-                        let mut operations = OperationHandler::generate(gen, geometry);
-                        OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                    })
-                },
-            );
-        });
-    group.finish();
-}
-
-pub fn greedy_over_foci<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/greedy_with_{}_devices", N));
-    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
-
-    [2].into_iter()
-        .chain((2..6).map(|i| i * i))
-        .for_each(|size| {
-            group.bench_with_input(
-                BenchmarkId::new("with_foci_num", size),
-                &generate_geometry(N),
-                |b, geometry| {
-                    let mut tx = TxDatagram::new(size);
-                    b.iter(|| {
-                        let gen = Greedy::<Sphere>::new(gen_foci(size, N))
-                            .operation_generator(geometry)
-                            .unwrap();
-                        let mut operations = OperationHandler::generate(gen, geometry);
-                        OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                    })
-                },
-            );
-        });
-    group.finish();
-}
-
-pub fn sdp_over_devices<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/sdp_with_{}_foci", N));
+pub fn lm<
+    D: Directivity + 'static,
+    B: LinAlgBackend<D> + 'static,
+    const NUM_DEV: usize,
+    const NUM_FOCI: usize,
+>(
+    c: &mut Criterion,
+) {
+    let mut group = c.benchmark_group("autd3-gain-holo");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
     let backend = B::new().unwrap();
-    (1..).take(5).for_each(|size| {
-        group.bench_with_input(
-            BenchmarkId::new("with_device_num", size * size),
-            &generate_geometry(size * size),
-            |b, geometry| {
-                let mut tx = TxDatagram::new(size);
-                b.iter(|| {
-                    let gen = SDP::new(backend.clone(), gen_foci(N, size * size))
-                        .operation_generator(geometry)
-                        .unwrap();
-                    let mut operations = OperationHandler::generate(gen, geometry);
-                    OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                })
-            },
-        );
-    });
+
+    group.bench_with_input(
+        format!("LM/{}_devices-{}-foci", NUM_DEV, NUM_FOCI),
+        &generate_geometry(NUM_DEV),
+        |b, geometry| {
+            let mut tx = TxDatagram::new(NUM_DEV);
+            b.iter(|| {
+                let gen = LM::new(backend.clone(), gen_foci(NUM_FOCI, NUM_DEV))
+                    .operation_generator(geometry)
+                    .unwrap();
+                let mut operations = OperationHandler::generate(gen, geometry);
+                OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
+            })
+        },
+    );
     group.finish();
 }
 
-pub fn naive_over_devices<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/naive_with_{}_foci", N));
+pub fn greedy<D: Directivity + 'static, const NUM_DEV: usize, const NUM_FOCI: usize>(
+    c: &mut Criterion,
+) {
+    let mut group = c.benchmark_group("autd3-gain-holo");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
-    let backend = B::new().unwrap();
-    (1..).take(5).for_each(|size| {
-        group.bench_with_input(
-            BenchmarkId::new("with_device_num", size * size),
-            &generate_geometry(size * size),
-            |b, geometry| {
-                let mut tx = TxDatagram::new(size);
-                b.iter(|| {
-                    let gen = Naive::new(backend.clone(), gen_foci(N, size * size))
-                        .operation_generator(geometry)
-                        .unwrap();
-                    let mut operations = OperationHandler::generate(gen, geometry);
-                    OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                })
-            },
-        );
-    });
-    group.finish();
-}
-
-pub fn gs_over_devices<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/gs_with_{}_foci", N));
-    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
-
-    let backend = B::new().unwrap();
-    (1..).take(5).for_each(|size| {
-        group.bench_with_input(
-            BenchmarkId::new("with_device_num", size * size),
-            &generate_geometry(size * size),
-            |b, geometry| {
-                let mut tx = TxDatagram::new(size);
-                b.iter(|| {
-                    let gen = GS::new(backend.clone(), gen_foci(N, size * size))
-                        .operation_generator(geometry)
-                        .unwrap();
-                    let mut operations = OperationHandler::generate(gen, geometry);
-                    OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                })
-            },
-        );
-    });
-    group.finish();
-}
-
-pub fn gspat_over_devices<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/gspat_with_{}_foci", N));
-    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
-
-    let backend = B::new().unwrap();
-    (1..).take(5).for_each(|size| {
-        group.bench_with_input(
-            BenchmarkId::new("with_device_num", size * size),
-            &generate_geometry(size * size),
-            |b, geometry| {
-                let mut tx = TxDatagram::new(size);
-                b.iter(|| {
-                    let gen = GSPAT::new(backend.clone(), gen_foci(N, size * size))
-                        .operation_generator(geometry)
-                        .unwrap();
-                    let mut operations = OperationHandler::generate(gen, geometry);
-                    OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                })
-            },
-        );
-    });
-    group.finish();
-}
-
-pub fn lm_over_devices<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/lm_with_{}_foci", N));
-    group
-        .sample_size(10)
-        .plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
-
-    let backend = B::new().unwrap();
-    (1..).take(3).for_each(|size| {
-        group.bench_with_input(
-            BenchmarkId::new("with_device_num", size * size),
-            &generate_geometry(size * size),
-            |b, geometry| {
-                let mut tx = TxDatagram::new(size);
-                b.iter(|| {
-                    let gen = LM::new(backend.clone(), gen_foci(N, size * size))
-                        .operation_generator(geometry)
-                        .unwrap();
-                    let mut operations = OperationHandler::generate(gen, geometry);
-                    OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                })
-            },
-        );
-    });
-    group.finish();
-}
-
-pub fn greedy_over_devices<B: LinAlgBackend<Sphere> + 'static, const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("autd3-gain-holo/greedy_with_{}_foci", N));
-    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
-
-    (1..).take(5).for_each(|size| {
-        group.bench_with_input(
-            BenchmarkId::new("with_device_num", size * size),
-            &generate_geometry(size * size),
-            |b, geometry| {
-                let mut tx = TxDatagram::new(size);
-                b.iter(|| {
-                    let gen = Greedy::<Sphere>::new(gen_foci(N, size * size))
-                        .operation_generator(geometry)
-                        .unwrap();
-                    let mut operations = OperationHandler::generate(gen, geometry);
-                    OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
-                })
-            },
-        );
-    });
+    group.bench_with_input(
+        format!("Greedy/{}_devices-{}-foci", NUM_DEV, NUM_FOCI),
+        &generate_geometry(NUM_DEV),
+        |b, geometry| {
+            let mut tx = TxDatagram::new(NUM_DEV);
+            b.iter(|| {
+                let gen = Greedy::<D>::new(gen_foci(NUM_FOCI, NUM_DEV))
+                    .operation_generator(geometry)
+                    .unwrap();
+                let mut operations = OperationHandler::generate(gen, geometry);
+                OperationHandler::pack(&mut operations, geometry, &mut tx, 0).unwrap();
+            })
+        },
+    );
     group.finish();
 }
