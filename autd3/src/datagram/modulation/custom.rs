955682b241e87584d07f056986ae53ea52b3bd75
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-06-14 18:34:57 +0900

    depr `Custom::with_sampling_config`

--- autd3/src/datagram/modulation/custom.rs
+++ autd3/src/datagram/modulation/custom.rs
@@ -3,6 +3,7 @@ use autd3_driver::derive::*;
 #[derive(Modulation, Clone, PartialEq, Debug)]
 pub struct Custom {
     buffer: Vec<u8>,
+    #[no_change]
     config: SamplingConfig,
     loop_behavior: LoopBehavior,
 }
@@ -15,6 +16,13 @@ impl Custom {
             loop_behavior: LoopBehavior::infinite(),
         }
     }
+
+    // GRCOV_EXCL_START
+    #[deprecated(note = "Do not change the sampling configuration", since = "25.3.0")]
+    pub fn with_sampling_config(self, config: SamplingConfig) -> Self {
+        Self { config, ..self }
+    }
+    // GRCOV_EXCL_STOP
 }
 impl Modulation for Custom {
     fn calc(&self, _: &Geometry) -> ModulationCalcResult {
