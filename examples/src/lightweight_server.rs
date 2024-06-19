85c5866a1a533c93960c479a669faa2771f8e413
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-06-19 13:18:18 +0900

    fix lightweight_server sample

--- examples/src/lightweight_server.rs
+++ examples/src/lightweight_server.rs
@@ -6,16 +6,20 @@ use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     let server = LightweightServer::new(|| {
         SOEM::builder()
             .with_timeout(std::time::Duration::from_millis(200))
             .with_err_handler(|slave, status| match status {
-                Status::Error(msg) => eprintln!("Error [{}]: {}", slave, msg),
-                Status::Lost(msg) => {
-                    eprintln!("Lost [{}]: {}", slave, msg);
+                Status::Error => eprintln!("Error [{}]: {}", slave, status),
+                Status::Lost => {
+                    eprintln!("Lost [{}]: {}", slave, status);
                     std::process::exit(-1);
                 }
-                Status::StateChanged(msg) => eprintln!("StateChanged [{}]: {}", slave, msg),
+                Status::StateChanged => eprintln!("StateChanged [{}]: {}", slave, status),
             })
     });
 
