859b382da3a8606f9d5c950a9e61e3d2ea538104
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-06-20 10:17:16 +0900

    add `Silencer::fixed_completion_time`

--- autd3-driver/src/datagram/mod.rs
+++ autd3-driver/src/datagram/mod.rs
@@ -33,7 +33,9 @@ pub use modulation::{
 pub use pulse_width_encoder::PulseWidthEncoder;
 pub use reads_fpga_state::ReadsFPGAState;
 pub use segment::SwapSegment;
-pub use silencer::{Silencer, SilencerFixedCompletionSteps, SilencerFixedUpdateRate};
+pub use silencer::{
+    Silencer, SilencerFixedCompletionSteps, SilencerFixedCompletionTime, SilencerFixedUpdateRate,
+};
 pub use stm::{FociSTM, GainSTM};
 pub use synchronize::Synchronize;
 pub use with_parallel_threshold::{
