2387e462ef865af19aef7a9712117ec1a04ca25f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-07-25 05:45:14 +0900

    update simulator example

--- examples/src/simulator.rs
+++ examples/src/simulator.rs
@@ -1,7 +1,5 @@
 mod tests;
 
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-
 use anyhow::Result;
 
 use autd3::prelude::*;
@@ -13,10 +11,7 @@ async fn main() -> Result<()> {
         AUTD3::new(Vector3::zeros()),
         AUTD3::new(Vector3::new(AUTD3::DEVICE_WIDTH, 0.0, 0.0)),
     ])
-    .open(Simulator::builder(SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::LOCALHOST),
-        8080,
-    )))
+    .open(Simulator::builder("127.0.0.1:8080".parse()?))
     .await?;
 
     tests::run(autd).await
