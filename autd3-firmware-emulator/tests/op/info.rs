3571f06aaedecbb0fdd02a33621542858518722b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-15 09:45:12 +0900

    add test for invalid info type

--- autd3-firmware-emulator/tests/op/info.rs
+++ autd3-firmware-emulator/tests/op/info.rs
@@ -1,6 +1,11 @@
 use autd3_driver::{
     datagram::*,
-    firmware::{cpu::TxDatagram, version::FirmwareVersion},
+    error::AUTDInternalError,
+    firmware::{
+        cpu::TxDatagram,
+        operation::{OperationGenerator, OperationHandler},
+        version::FirmwareVersion,
+    },
 };
 use autd3_firmware_emulator::CPUEmulator;
 
@@ -43,6 +48,27 @@ fn send_firminfo() -> anyhow::Result<()> {
 
     send(&mut cpu, FetchFirmInfo::Clear, &geometry, &mut tx)?;
     assert!(cpu.reads_fpga_state());
+
+    Ok(())
+}
+
+#[test]
+fn invalid_info_type() -> anyhow::Result<()> {
+    let geometry = create_geometry(1);
+    let mut cpu = CPUEmulator::new(0, geometry.num_transducers());
+    let mut tx = TxDatagram::new(geometry.num_devices());
+
+    let d = FetchFirmInfo::CPUMajor;
+    let (op, op_null) = d.operation_generator(&geometry)?.generate(&geometry[0]);
+
+    OperationHandler::pack(&mut [(op, op_null)], &geometry, &mut tx, usize::MAX)?;
+    tx[0].payload[1] = 7;
+
+    cpu.send(&tx);
+    assert_eq!(
+        Err(AUTDInternalError::InvalidInfoType),
+        Result::<(), AUTDInternalError>::from(&cpu.rx())
+    );
 
     Ok(())
 }
