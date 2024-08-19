9ebfb31581fcaf8f30467624f291df15e068af11
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-20 08:06:32 +0900

    update autd3 prelude

--- autd3/src/prelude.rs
+++ autd3/src/prelude.rs
@@ -9,11 +9,11 @@ pub use crate::{
 pub use autd3_driver::{
     autd3_device::AUTD3,
     datagram::{
-        Clear, DebugSettings, EmulateGPIOIn, FociSTM, ForceFan, GainSTM,
-        IntoDatagramWithParallelThreshold, IntoDatagramWithSegment,
-        IntoDatagramWithSegmentTransition, IntoDatagramWithTimeout, IntoGainCache,
-        IntoGainTransform, IntoModulationCache, IntoModulationTransform, IntoRadiationPressure,
-        Modulation, ModulationProperty, PulseWidthEncoder, ReadsFPGAState, Silencer, SwapSegment,
+        Clear, DebugSettings, FociSTM, ForceFan, GainSTM, IntoDatagramWithParallelThreshold,
+        IntoDatagramWithSegment, IntoDatagramWithSegmentTransition, IntoDatagramWithTimeout,
+        IntoGainCache, IntoGainTransform, IntoModulationCache, IntoModulationTransform,
+        IntoRadiationPressure, Modulation, ModulationProperty, PulseWidthEncoder, ReadsFPGAState,
+        Silencer, SwapSegment,
     },
     defined::{
         deg, kHz, mm, rad, ControlPoint, ControlPoints, Hz, PI, ULTRASOUND_FREQ, ULTRASOUND_PERIOD,
