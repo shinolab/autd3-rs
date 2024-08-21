9857bef4e528fa2e0e922dd156e36735ca5b24d5
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-22 06:54:31 +0900

    refactor autd3 tests

--- autd3/tests/link/audit.rs
+++ autd3/tests/link/audit.rs
@@ -1,91 +1,73 @@
+use std::time::Duration;
+
 use autd3::{derive::DEFAULT_TIMEOUT, link::Audit, prelude::*};
-use autd3_driver::{
-    firmware::{cpu::RxMessage, fpga::FPGAState},
-    link::Link,
-};
+use autd3_driver::firmware::{cpu::RxMessage, fpga::FPGAState};
 
 #[tokio::test]
 async fn audit_test() -> anyhow::Result<()> {
     let mut autd = Controller::builder([AUTD3::new(Vector3::zeros())])
-        .open(Audit::builder().with_timeout(std::time::Duration::from_millis(100)))
+        .open(Audit::builder().with_timeout(Duration::from_millis(100)))
         .await?;
-    assert_eq!(std::time::Duration::from_millis(100), autd.link().timeout());
-
+    assert_eq!(Duration::from_millis(100), autd.link().timeout());
     assert_eq!(0, autd.link()[0].idx());
     assert_eq!(DEFAULT_TIMEOUT, autd.link().last_timeout());
-
-    assert_eq!(vec![None], autd.fpga_state().await?);
-    autd.send(ReadsFPGAState::new(|_| true)).await?;
-    autd.link_mut()[0].update();
-    assert_eq!(
-        vec![Option::<FPGAState>::from(&RxMessage::new(0x00, 0x88))],
-        autd.fpga_state().await?
-    );
-    autd.link_mut()[0].fpga_mut().assert_thermal_sensor();
-    autd.link_mut()[0].update();
-    assert_eq!(
-        vec![Option::<FPGAState>::from(&RxMessage::new(0x00, 0x89))],
-        autd.fpga_state().await?
-    );
-
-    autd.link_mut().down();
-    assert_eq!(
-        Err(AUTDError::Internal(AUTDInternalError::SendDataFailed)),
-        autd.send(Static::new()).await
-    );
-    assert_eq!(Err(AUTDError::ReadFPGAStateFailed), autd.fpga_state().await);
-    autd.link_mut().up();
-    autd.send(Static::new()).await?;
-    autd.link_mut().break_down();
-    assert_eq!(
-        Err(AUTDError::Internal(AUTDInternalError::LinkError(
-            "broken".to_string()
-        ))),
-        autd.send(Static::new()).await
-    );
-    assert_eq!(
-        Err(AUTDError::Internal(AUTDInternalError::LinkError(
-            "broken".to_string()
-        ))),
-        autd.fpga_state().await
-    );
-    autd.link_mut().repair();
-    autd.send(Static::new()).await?;
-
-    autd.close().await?;
-    assert_eq!(
-        Err(AUTDError::Internal(AUTDInternalError::LinkClosed)),
-        autd.send(Static::new()).await
-    );
-    assert_eq!(
-        Err(AUTDError::Internal(AUTDInternalError::LinkClosed)),
-        autd.fpga_state().await
-    );
-
-    Ok(())
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn last_parallel_threshold() -> anyhow::Result<()> {
-    let mut autd =
-        Controller::builder([AUTD3::new(Vector3::zeros()), AUTD3::new(Vector3::zeros())])
-            .with_parallel_threshold(0)
-            .open(Audit::builder())
-            .await?;
     assert_eq!(usize::MAX, autd.link().last_parallel_threshold());
 
-    autd.send(Null::new()).await?;
-    assert_eq!(0, autd.link().last_parallel_threshold());
+    // test last_timeout and last_parallel_threshold
+    {
+        assert!(autd.send(Null::new()).await.is_ok());
+        assert_eq!(Duration::from_millis(100), autd.link().last_timeout());
+        assert_eq!(4, autd.link().last_parallel_threshold());
 
-    autd.send(Static::new()).await?;
-    assert_eq!(usize::MAX, autd.link().last_parallel_threshold());
+        assert!(autd.send(Static::new()).await.is_ok());
+        assert_eq!(DEFAULT_TIMEOUT, autd.link().last_timeout());
+        assert_eq!(usize::MAX, autd.link().last_parallel_threshold());
+    }
 
-    autd.send(Static::new().with_parallel_threshold(10)).await?;
-    assert_eq!(10, autd.link().last_parallel_threshold());
+    // test fpga_state
+    {
+        assert_eq!(vec![None], autd.fpga_state().await?);
+        assert!(autd.send(ReadsFPGAState::new(|_| true)).await.is_ok());
+        autd.link_mut()[0].update();
+        assert_eq!(
+            vec![Option::<FPGAState>::from(&RxMessage::new(0x00, 0x88))],
+            autd.fpga_state().await?
+        );
+        autd.link_mut()[0].fpga_mut().assert_thermal_sensor();
+        autd.link_mut()[0].update();
+        assert_eq!(
+            vec![Option::<FPGAState>::from(&RxMessage::new(0x00, 0x89))],
+            autd.fpga_state().await?
+        );
+    }
 
-    autd.send((Static::new(), Static::new()).with_parallel_threshold(5))
-        .await?;
-    assert_eq!(5, autd.link().last_parallel_threshold());
+    {
+        autd.link_mut().down();
+        assert_eq!(
+            Err(AUTDError::Internal(AUTDInternalError::SendDataFailed)),
+            autd.send(Static::new()).await
+        );
+        assert_eq!(Err(AUTDError::ReadFPGAStateFailed), autd.fpga_state().await);
+        autd.link_mut().up();
+        assert!(autd.send(Static::new()).await.is_ok());
+        autd.link_mut().break_down();
+        assert_eq!(
+            Err(AUTDError::Internal(AUTDInternalError::LinkError(
+                "broken".to_string()
+            ))),
+            autd.send(Static::new()).await
+        );
+        autd.link_mut().repair();
+        assert!(autd.send(Static::new()).await.is_ok());
+    }
+
+    {
+        assert!(autd.close().await.is_ok());
+        assert_eq!(
+            Err(AUTDError::Internal(AUTDInternalError::LinkClosed)),
+            autd.send(Static::new()).await
+        );
+    }
 
     Ok(())
 }
