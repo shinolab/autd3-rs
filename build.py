ea5301d71518ee60bebce062f8d640de0d5c5195
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-23 09:54:52 +0900

    exclude #[error from cov

--- build.py
+++ build.py
@@ -333,7 +333,7 @@ def rust_coverage(args):
                 "-t",
                 args.format,
                 "--excl-line",
-                r"^\s*((debug_)?assert(_eq|_ne)?!|#\[derive\()|GRCOV_EXCL_LINE|unreachable!|unimplemented!|tracing::",
+                r"^\s*(debug_)?assert(_eq|_ne)?!|#\[derive|GRCOV_EXCL_LINE|unreachable!|unimplemented!|tracing::|#\[error",
                 "--ignore",
                 "autd3-derive/**/*",
                 "--ignore",
