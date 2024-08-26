bc11a2c9b5fcf5cd6692f56cd1aa4903298a831f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-26 14:18:08 +0900

    update examples

--- examples/src/tests/holo.rs
+++ examples/src/tests/holo.rs
@@ -7,67 +7,37 @@ use std::io::{self, Write};
 pub async fn holo(autd: &mut Controller<impl Link>) -> anyhow::Result<bool> {
     autd.send(Silencer::default()).await?;
 
-    let m = Sine::new(150. * Hz);
-
     let center = autd.geometry().center() + Vector3::new(0., 0., 150.0 * mm);
     let p = Vector3::new(30. * mm, 0., 0.);
+    let backend = NalgebraBackend::<Sphere>::new().unwrap();
+    let target_amp = 2.5e3 * autd.geometry().num_devices() as f32 * Pa;
+    let foci = [(center + p, target_amp), (center - p, target_amp)];
 
-    println!("[0]: GS");
-    println!("[1]: GSPAT");
-    println!("[2]: LSS");
-    println!("[3]: LM");
-    println!("[4]: Greedy");
-    println!("[Others]: GS-PAT");
+    let mut gains: Vec<(&str, Box<dyn autd3::driver::datagram::Gain>)> = vec![
+        ("GS", Box::new(GS::new(backend.clone(), foci.clone()))),
+        ("GSPAT", Box::new(GSPAT::new(backend.clone(), foci.clone()))),
+        ("Naive", Box::new(Naive::new(backend.clone(), foci.clone()))),
+        ("LM", Box::new(LM::new(backend.clone(), foci.clone()))),
+        ("Greedy", Box::new(Greedy::<Sphere>::new(foci.clone()))),
+    ];
+
+    gains.iter().enumerate().for_each(|(i, (name, _))| {
+        println!("[{}]: {}", i, name);
+    });
+    println!("[Others]: GSPAT");
     print!("{}", "Choose number: ".green().bold());
     io::stdout().flush()?;
 
     let mut s = String::new();
     io::stdin().read_line(&mut s)?;
-
-    let backend = NalgebraBackend::<Sphere>::new().unwrap();
-
-    let target_amp = 2.5e3 * autd.geometry().num_devices() as f32 * Pa;
-    match s.trim().parse::<usize>() {
-        Ok(0) => {
-            let g = GS::new(
-                backend,
-                [(center + p, target_amp), (center - p, target_amp)],
-            );
-            autd.send((m, g)).await?
-        }
-        Ok(1) => {
-            let g = GSPAT::new(
-                backend,
-                [(center + p, target_amp), (center - p, target_amp)],
-            );
-            autd.send((m, g)).await?
-        }
-        Ok(2) => {
-            let g = LSS::new(
-                backend,
-                [(center + p, target_amp), (center - p, target_amp)],
-            );
-            autd.send((m, g)).await?
-        }
-        Ok(3) => {
-            let g = LM::new(
-                backend,
-                [(center + p, target_amp), (center - p, target_amp)],
-            );
-            autd.send((m, g)).await?
-        }
-        Ok(4) => {
-            let g = Greedy::<Sphere>::new([(center + p, target_amp), (center - p, target_amp)]);
-            autd.send((m, g)).await?
-        }
-        _ => {
-            let g = GSPAT::new(
-                backend,
-                [(center + p, target_amp), (center - p, target_amp)],
-            );
-            autd.send((m, g)).await?
-        }
+    let idx = match s.trim().parse::<usize>() {
+        Ok(i) if i < gains.len() => i,
+        _ => 1,
     };
+
+    let m = Sine::new(150. * Hz);
+    let g = gains.swap_remove(idx).1;
+    autd.send((m, g)).await?;
 
     Ok(true)
 }
