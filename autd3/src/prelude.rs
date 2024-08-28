4e68afc1b1e64c02b6fadd7fd0a1af4ce92622cf
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-29 08:48:54 +0900

    rm 'Transform' modulation

--- autd3/src/prelude.rs
+++ autd3/src/prelude.rs
@@ -3,9 +3,7 @@ pub use crate::{
     error::AUTDError,
     gain::{Bessel, Focus, Group, IntoGainCache, IntoGainTransform, Null, Plane, Uniform},
     link::Nop,
-    modulation::{
-        IntoModulationCache, IntoModulationTransform, IntoRadiationPressure, Sine, Square, Static,
-    },
+    modulation::{IntoModulationCache, IntoRadiationPressure, Sine, Square, Static},
 };
 
 pub use autd3_driver::{
