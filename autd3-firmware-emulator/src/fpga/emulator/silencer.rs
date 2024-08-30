79d9278510bf3be79732d31a71ac60bd706f2d68
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-30 10:53:45 +0900

    add `apply_silencer_fixed_completion_steps` test

--- autd3-firmware-emulator/src/fpga/emulator/silencer.rs
+++ autd3-firmware-emulator/src/fpga/emulator/silencer.rs
@@ -194,4 +194,51 @@ mod tests {
             fpga.apply_silencer_helper(&input, phase, 1, initial)
         );
     }
+
+    #[rstest::rstest]
+    #[test]
+    #[case(vec![21, 33, 45, 57, 69, 81, 93, 105, 117, 128, 128], 10, false, 10, vec![128; 11])] //  case 1: intensity
+    #[case(vec![21, 33, 45, 57, 69, 81, 93, 105, 117, 128, 128], 10, true, 10, vec![128; 11])] // case 1: phase
+    #[case(vec![25, 51, 77, 103, 129, 155, 180, 205, 230, 255, 255], 10, false, 0, vec![255; 11])] // case 2: intensity
+    #[case(vec![12, 25, 38, 51, 64, 77, 90, 103, 116, 128, 128], 10, true, 0, vec![128; 11])] // case 2: phase
+    #[case(vec![25, 51, 77, 103, 129, 155, 180, 205, 230, 255, 255], 10, false, 0, vec![255; 11])] // case 3: intensity
+    #[case(vec![254, 241, 228, 215, 202, 189, 176, 163, 151, 139, 139], 10, true, 10, vec![139; 11])] // case 3: phase
+    #[case(vec![25, 51, 77, 103, 129, 155, 180, 205, 230, 255, 255], 10, false, 0, vec![255; 11])] // case 4: intensity
+    #[case(vec![244, 231, 218, 205, 192, 179, 166, 153, 141, 129, 129], 10, true, 0, vec![129; 11])] // case 4: phase
+    #[case(vec![25, 51, 77, 103, 129, 155, 180, 205, 230, 255, 255], 10, false, 0, vec![255; 11])] // case 5: intensity
+    #[case(vec![249, 241, 233, 225, 217, 209, 201, 194, 187, 180, 180], 10, true, 0, vec![180; 11])] // case 5: phase
+    #[case(vec![254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 245], 10, false, 255, vec![245; 11])] // case 6: intensity
+    #[case(vec![175, 169, 163, 158, 153, 148, 143, 138, 133, 128, 128], 10, true, 180, vec![128; 11])] // case 6: phase
+    #[case(vec![254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 245], 10, false, 255, vec![245; 11])] // case 7: intensity
+    #[case(vec![248, 240, 232, 224, 216, 208, 201, 194, 187, 180, 180], 10, true, 255, vec![180; 11])] // case 7: phase
+    #[case(vec![254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 245], 10, false, 255, vec![245; 11])] // case 8: intensity
+    #[case(vec![11, 24, 37, 50, 63, 76, 89, 102, 114, 126, 126], 10, true, 255, vec![126; 11])] // case 8: phase
+    #[case(vec![254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 245], 10, false, 255, vec![245; 11])] // case 9: intensity
+    #[case(vec![243, 230, 217, 204, 191, 178, 165, 152, 139, 127, 127], 10, true, 255, vec![127; 11])] // case 9: phase
+    #[case(vec![254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 245], 10, false, 255, vec![245; 11])] // case 10: intensity
+    #[case(vec![0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10], 10, true, 255, vec![10; 11])] // case 10: phase
+    #[case(vec![254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 245], 10, false, 255, vec![245; 11])] // case 11: intensity
+    #[case(vec![187, 195, 203, 211, 219, 227, 235, 242, 249, 0, 0], 10, true, 180, vec![0; 11])] // case 11: phase
+    #[case(vec![0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5], 10, false, 0, vec![5; 11])] // case 12: intensity
+    #[case(vec![0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5], 10, true, 0, vec![5; 11])] // case 12: phase
+    #[cfg_attr(miri, ignore)]
+    fn apply_silencer_fixed_completion_steps(
+        #[case] expect: Vec<u8>,
+        #[case] value: u8,
+        #[case] phase: bool,
+        #[case] initial: u8,
+        #[case] input: Vec<u8>,
+    ) {
+        let fpga = FPGAEmulator::new(249);
+        if phase {
+            fpga.mem.controller_bram_mut()[ADDR_SILENCER_COMPLETION_STEPS_PHASE] = value as _;
+        } else {
+            fpga.mem.controller_bram_mut()[ADDR_SILENCER_COMPLETION_STEPS_INTENSITY] = value as _;
+        }
+        assert_eq!(
+            expect,
+            fpga.apply_silencer_helper(&input, phase, 1, initial)
+        );
+    }
+
 }
