51158748e42e713a3b34ca889389ce4b05664dbc
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-09-04 15:14:55 +0900

    add tests for link-calc

--- autd3-link-calc/src/error.rs
+++ autd3-link-calc/src/error.rs
@@ -1,7 +1,7 @@
-use autd3_driver::defined::ULTRASOUND_PERIOD;
+use autd3_driver::{defined::ULTRASOUND_PERIOD, error::AUTDInternalError};
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, Clone, PartialEq)]
 pub enum CalcError {
     #[error("Recording is already started")]
     RecordingAlreadyStarted,
@@ -9,4 +9,12 @@ pub enum CalcError {
     RecodingNotStarted,
     #[error("Tick must be multiple of {:?}", ULTRASOUND_PERIOD)]
     InvalidTick,
+    #[error("Invalid operation when recording")]
+    InvalidOperationWhenRecording,
+}
+
+impl From<CalcError> for AUTDInternalError {
+    fn from(value: CalcError) -> Self {
+        AUTDInternalError::LinkError(value.to_string())
+    }
 }
