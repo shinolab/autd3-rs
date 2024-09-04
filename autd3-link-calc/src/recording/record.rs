4db2520f0b4d26154495671974f5228074e09341
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-09-04 14:04:00 +0900

    refactor recoding of link-calc

--- autd3-link-calc/src/recording/record.rs
+++ autd3-link-calc/src/recording/record.rs
@@ -5,71 +5,82 @@ use autd3_driver::{
     derive::Builder,
     ethercat::DcSysTime,
     firmware::fpga::{Drive, SilencerTarget},
-    geometry::Transducer,
 };
 
+use autd3_firmware_emulator::FPGAEmulator;
 use derive_more::Deref;
 
-use crate::Calc;
+pub(crate) struct RawTransducerRecord {
+    pub drive: Vec<Drive>,
+    pub modulation: Vec<u8>,
+}
 
-#[derive(Debug, Clone, Builder)]
-pub struct TransducerRecord {
+pub(crate) struct RawDeviceRecord {
+    pub(crate) records: Vec<RawTransducerRecord>,
+}
+
+pub(crate) struct RawRecord {
+    pub records: Vec<RawDeviceRecord>,
+    pub start: DcSysTime,
+    pub current: DcSysTime,
+}
+
+#[derive(Builder)]
+pub struct TransducerRecord<'a> {
     #[get]
     pub(crate) drive: Vec<Drive>,
     #[get]
     pub(crate) modulation: Vec<u8>,
+    pub(crate) fpga: &'a FPGAEmulator,
 }
 
-#[derive(Debug, Clone, Deref, Builder)]
-pub struct DeviceRecord {
-    #[get]
+#[derive(Deref)]
+pub struct DeviceRecord<'a> {
     #[deref]
-    pub(crate) records: Vec<TransducerRecord>,
+    pub(crate) records: Vec<TransducerRecord<'a>>,
 }
 
-#[derive(Debug, Clone, Deref, Builder)]
-pub struct Record {
+#[derive(Deref, Builder)]
+pub struct Record<'a> {
     #[deref]
-    pub(crate) records: Vec<DeviceRecord>,
+    pub(crate) records: Vec<DeviceRecord<'a>>,
     #[get]
     pub(crate) start: DcSysTime,
     #[get]
     pub(crate) end: DcSysTime,
 }
 
-impl Calc {
-    pub fn pulse_width(&self, record: &Record, tr: &Transducer) -> Vec<(Duration, u8)> {
-        let record = &record.records[tr.dev_idx()][tr.idx()];
-        let cpu = &self.sub_devices[tr.dev_idx()].cpu;
-        match cpu.fpga().silencer_target() {
+impl<'a> TransducerRecord<'a> {
+    pub fn pulse_width(&self) -> Vec<(Duration, u8)> {
+        let fpga = &self.fpga;
+        match fpga.silencer_target() {
             SilencerTarget::Intensity => {
-                let intensity = record
+                let intensity = self
                     .drive
                     .iter()
-                    .zip(record.modulation.iter())
+                    .zip(self.modulation.iter())
                     .map(|(d, &m)| ((d.intensity().value() as u16 * m as u16) / 255) as u8)
                     .collect::<Vec<_>>();
-                let intensity = cpu.fpga().apply_silencer(0, &intensity, false);
+                let intensity = fpga.apply_silencer(0, &intensity, false);
                 intensity
                     .into_iter()
                     .enumerate()
                     .map(|(i, intensity)| {
                         (
                             i as u32 * ULTRASOUND_PERIOD,
-                            cpu.fpga().pulse_width_encoder_table_at(intensity as _),
+                            fpga.pulse_width_encoder_table_at(intensity as _),
                         )
                     })
                     .collect()
             }
             SilencerTarget::PulseWidth => {
-                let pulse_width = record
+                let pulse_width = self
                     .drive
                     .iter()
-                    .zip(record.modulation.iter())
-                    .map(|(d, &m)| cpu.fpga().to_pulse_width(d.intensity(), m))
+                    .zip(self.modulation.iter())
+                    .map(|(d, &m)| fpga.to_pulse_width(d.intensity(), m))
                     .collect::<Vec<_>>();
-                cpu.fpga()
-                    .apply_silencer(0, &pulse_width, false)
+                fpga.apply_silencer(0, &pulse_width, false)
                     .into_iter()
                     .enumerate()
                     .map(|(i, pw)| (i as u32 * ULTRASOUND_PERIOD, pw))
