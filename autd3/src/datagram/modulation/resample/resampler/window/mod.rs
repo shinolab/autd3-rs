ed258681ded7f09bf5659c647432f50c6e3c92ce
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-09-09 10:41:13 +0900

    make `Resampler` implement `Send` and `Sync`

--- autd3/src/datagram/modulation/resample/resampler/window/mod.rs
+++ autd3/src/datagram/modulation/resample/resampler/window/mod.rs
@@ -4,7 +4,7 @@ mod rectangular;
 pub use blackman::Blackman;
 pub use rectangular::Rectangular;
 
-pub trait InterpolationWindow: std::fmt::Debug + Clone + Copy + PartialEq {
+pub trait InterpolationWindow: std::fmt::Debug + Clone + Copy + PartialEq + Send + Sync {
     fn window_size(&self) -> usize;
     fn value(&self, k: usize) -> f64;
 }
