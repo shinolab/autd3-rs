093eddbd3f045b57440745d157a2fc32f6600e3e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-09-09 10:13:52 +0900

    make `WithSampling` pub

--- autd3-driver/src/datagram/mod.rs
+++ autd3-driver/src/datagram/mod.rs
@@ -30,7 +30,7 @@ pub use modulation::{
 pub use pulse_width_encoder::PulseWidthEncoder;
 pub use reads_fpga_state::ReadsFPGAState;
 pub use segment::SwapSegment;
-pub use silencer::{FixedCompletionTime, FixedUpdateRate, Silencer};
+pub use silencer::{FixedCompletionTime, FixedUpdateRate, Silencer, WithSampling};
 pub use stm::{FociSTM, GainSTM, STMConfig, STMConfigNearest};
 pub use synchronize::Synchronize;
 pub use with_parallel_threshold::{
