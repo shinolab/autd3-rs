8d8e66189bd55cd34b17be8755f0da81c62aa9c0
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-09-09 10:41:13 +0900

    make `Resampler` implement `Send` and `Sync`

--- autd3/src/datagram/modulation/resample/resampler/mod.rs
+++ autd3/src/datagram/modulation/resample/resampler/mod.rs
@@ -5,7 +5,7 @@ use autd3_driver::{defined::Freq, derive::SamplingConfig};
 pub use sinc::SincInterpolation;
 pub use window::*;
 
-pub trait Resampler: std::fmt::Debug {
+pub trait Resampler: std::fmt::Debug + Send + Sync {
     fn upsample(&self, buffer: &[u8], ratio: f64) -> Vec<u8>;
     fn downsample(&self, buffer: &[u8], ratio: f64) -> Vec<u8>;
     fn resample(&self, buffer: &[u8], source: Freq<f32>, target: SamplingConfig) -> Vec<u8> {
