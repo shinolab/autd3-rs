0f929781390396746e919eedeb88f53773c66031
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-05 10:46:52 +0900

    handle out of range value in `Sine` and `Fourier`

--- autd3/src/datagram/modulation/fourier.rs
+++ autd3/src/datagram/modulation/fourier.rs
@@ -7,18 +7,27 @@ use autd3_driver::derive::*;
 use derive_more::Deref;
 use num::integer::lcm;
 
-#[derive(Modulation, Clone, PartialEq, Debug, Deref)]
+#[derive(Modulation, Clone, PartialEq, Debug, Deref, Builder)]
 pub struct Fourier<S: SamplingMode> {
     #[deref]
     components: Vec<Sine<S>>,
     #[no_change]
     config: SamplingConfig,
+    #[get]
+    #[set]
+    scale_factor: Option<f32>,
+    #[get]
+    #[set]
+    clamp: bool,
     loop_behavior: LoopBehavior,
 }
 
 impl<S: SamplingMode> Fourier<S> {
     pub fn new(componens: impl IntoIterator<Item = Sine<S>>) -> Result<Self, AUTDInternalError> {
-        let components = componens.into_iter().collect::<Vec<_>>();
+        let components = componens
+            .into_iter()
+            .map(|s| s.with_clamp(false))
+            .collect::<Vec<_>>();
         let config = components
             .first()
             .ok_or(AUTDInternalError::ModulationError(
@@ -37,6 +46,8 @@ impl<S: SamplingMode> Fourier<S> {
         Ok(Self {
             config,
             components,
+            scale_factor: None,
+            clamp: false,
             loop_behavior: LoopBehavior::infinite(),
         })
     }
@@ -47,22 +58,36 @@ impl<S: SamplingMode> Modulation for Fourier<S> {
         let buffers = self
             .components
             .iter()
-            .map(|c| c.calc())
-            .collect::<Result<Vec<_>, _>>()?;
-        let num_components = buffers.len();
-        let res = vec![0usize; buffers.iter().fold(1, |acc, x| lcm(acc, x.len()))];
+            .map(|c| Ok(c.calc_raw()?.collect::<Vec<_>>()))
+            .collect::<Result<Vec<_>, AUTDInternalError>>()?;
+        let scale = self.scale_factor.unwrap_or(1. / buffers.len() as f32);
+        let res = vec![0f32; buffers.iter().fold(1, |acc, x| lcm(acc, x.len()))];
         Ok(Arc::new(
             buffers
                 .into_iter()
                 .fold(res, |mut acc, x| {
                     acc.iter_mut()
                         .zip(x.iter().cycle())
-                        .for_each(|(a, &b)| *a += b as usize);
+                        .for_each(|(a, &b)| *a += b);
                     acc
                 })
                 .into_iter()
-                .map(|x| (x / num_components) as u8)
-                .collect(),
+                .map(|x| (x * scale).round() as isize)
+                .map(|v| {
+                    if (u8::MIN as _..=u8::MAX as _).contains(&v) {
+                        Ok(v as _)
+                    } else if self.clamp {
+                        Ok(v.clamp(u8::MIN as _, u8::MAX as _) as _)
+                    } else {
+                        Err(AUTDInternalError::ModulationError(format!(
+                            "Fourier modulation value ({}) is out of range [{}, {}]",
+                            v,
+                            u8::MIN,
+                            u8::MAX,
+                        )))?
+                    }
+                })
+                .collect::<Result<Vec<_>, AUTDInternalError>>()?,
         ))
     }
 }
@@ -83,11 +108,11 @@ mod tests {
         let f3 = Sine::new(200. * Hz);
         let f4 = Sine::new(250. * Hz);
 
-        let f0_buf = &f0.calc()?;
-        let f1_buf = &f1.calc()?;
-        let f2_buf = &f2.calc()?;
-        let f3_buf = &f3.calc()?;
-        let f4_buf = &f4.calc()?;
+        let f0_buf = f0.calc_raw()?.collect::<Vec<_>>();
+        let f1_buf = f1.calc_raw()?.collect::<Vec<_>>();
+        let f2_buf = f2.calc_raw()?.collect::<Vec<_>>();
+        let f3_buf = f3.calc_raw()?.collect::<Vec<_>>();
+        let f4_buf = f4.calc_raw()?.collect::<Vec<_>>();
 
         let f = Fourier::new([f0, f1, f2, f3, f4])?;
 
@@ -108,12 +133,13 @@ mod tests {
         (0..buf.len()).for_each(|i| {
             assert_eq!(
                 buf[i],
-                ((f0_buf[i % f0_buf.len()] as usize
-                    + f1_buf[i % f1_buf.len()] as usize
-                    + f2_buf[i % f2_buf.len()] as usize
-                    + f3_buf[i % f3_buf.len()] as usize
-                    + f4_buf[i % f4_buf.len()] as usize)
-                    / 5) as u8
+                ((f0_buf[i % f0_buf.len()]
+                    + f1_buf[i % f1_buf.len()]
+                    + f2_buf[i % f2_buf.len()]
+                    + f3_buf[i % f3_buf.len()]
+                    + f4_buf[i % f4_buf.len()])
+                    / 5.)
+                    .round() as u8
             );
         });
 
@@ -141,6 +167,42 @@ mod tests {
                 "Components must not be empty".to_string()
             )),
             Fourier::<ExactFreq>::new(vec![])
+        );
+    }
+
+    #[rstest::rstest]
+    #[case(
+        Err(AUTDInternalError::ModulationError("Fourier modulation value (-39) is out of range [0, 255]".to_owned())),
+        0x00,
+        false,
+        None
+    )]
+    #[case(
+        Ok(Arc::new(vec![0, 39, 75, 103, 121, 128, 121, 103, 75, 39, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])),
+        0x00,
+        true,
+        None
+    )]
+    #[case(
+        Err(AUTDInternalError::ModulationError("Fourier modulation value (334) is out of range [0, 255]".to_owned())),
+        0xFF,
+        false,
+        Some(2.)
+    )]
+    #[test]
+    fn out_of_range(
+        #[case] expect: Result<Arc<Vec<u8>>, AUTDInternalError>,
+        #[case] offset: u8,
+        #[case] clamp: bool,
+        #[case] scale: Option<f32>,
+    ) {
+        assert_eq!(
+            expect,
+            Fourier::new([Sine::new(200 * Hz).with_offset(offset)])
+                .unwrap()
+                .with_clamp(clamp)
+                .with_scale_factor(scale)
+                .calc()
         );
     }
 }
