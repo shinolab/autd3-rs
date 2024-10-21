236bb719e79414c95e890c2c9566a8f47f2f915e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-21 16:46:24 +0900

    update soem example

--- examples/src/soem.rs
+++ examples/src/soem.rs
@@ -12,16 +12,13 @@ async fn main() -> Result<()> {
         .init();
 
     let autd = Controller::builder([AUTD3::new(Vector3::zeros())])
-        .open(
-            SOEM::builder().with_err_handler(|slave, status| match status {
-                Status::Lost => {
-                    eprintln!("slave[{}]: {}", slave, status);
-                    // You can also wait for the link to recover, without exitting the process
-                    std::process::exit(-1);
-                }
-                _ => eprintln!("slave[{}]: {}", slave, status),
-            }),
-        )
+        .open(SOEM::builder().with_err_handler(|slave, status| {
+            eprintln!("slave[{}]: {}", slave, status);
+            if status == Status::Lost {
+                // You can also wait for the link to recover, without exitting the process
+                std::process::exit(-1);
+            }
+        }))
         .await?;
 
     tests::run(autd).await
