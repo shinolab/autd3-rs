2fc50f5a23d3fd5cf640839cedec7681c3c52762
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-01 07:56:30 +0900

    purge `autd3-link-soem`

--- autd3-link-soem/src/local/timer_strategy.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, Clone, Copy, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[repr(u8)]
-pub enum TimerStrategy {
-    SpinSleep = 0,
-    StdSleep = 1,
-    SpinWait = 2,
-}
