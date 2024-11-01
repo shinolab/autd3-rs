5ad41c5b2509a5b4f84db4ee09b6a8d115c22171
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-01 09:30:36 +0900

    rm soem

--- examples/src/lightweight_server.rs
+++ examples/src/lightweight_server.rs
@@ -1,4 +1,4 @@
-use autd3_link_soem::{Status, SOEM};
+use autd3_link_twincat::TwinCAT;
 use autd3_protobuf::{lightweight::LightweightServer, *};
 
 use tokio::sync::mpsc;
@@ -10,18 +10,7 @@ async fn main() -> anyhow::Result<()> {
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    let server = LightweightServer::new(|| {
-        SOEM::builder()
-            .with_timeout(std::time::Duration::from_millis(200))
-            .with_err_handler(|slave, status| match status {
-                Status::Error => eprintln!("Error [{}]: {}", slave, status),
-                Status::Lost => {
-                    eprintln!("Lost [{}]: {}", slave, status);
-                    std::process::exit(-1);
-                }
-                Status::StateChanged => eprintln!("StateChanged [{}]: {}", slave, status),
-            })
-    });
+    let server = LightweightServer::new(|| TwinCAT::builder());
 
     let (tx, mut rx) = mpsc::channel(1);
     ctrlc_async::set_async_handler(async move {
