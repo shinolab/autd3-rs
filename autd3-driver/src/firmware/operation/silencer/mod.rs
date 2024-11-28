f8e595e1f5a4bb8e04c5cfbc5390e7ab66967352
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-29 07:59:22 +0900

    add `SilencerControlFlags`

--- autd3-driver/src/firmware/operation/silencer/mod.rs
+++ autd3-driver/src/firmware/operation/silencer/mod.rs
@@ -1,11 +1,19 @@
 mod completion_steps;
 mod update_rate;
 
-pub const SILENCER_FLAG_BIT_FIXED_UPDATE_RATE_MODE: u8 = 0;
-pub const SILENCER_FLAG_FIXED_UPDATE_RATE_MODE: u8 = 1 << SILENCER_FLAG_BIT_FIXED_UPDATE_RATE_MODE;
-pub const SILENCER_FLAG_BIT_PULSE_WIDTH: u8 = 1;
-pub const SILENCER_FLAG_PULSE_WIDTH: u8 = 1 << SILENCER_FLAG_BIT_PULSE_WIDTH;
-const SILENCER_FLAG_STRICT_MODE: u8 = 1 << 2;
+#[derive(Clone, Copy, PartialEq, Debug, IntoBytes, Immutable)]
+#[repr(C)]
+pub struct SilencerControlFlags(u8);
+
+bitflags::bitflags! {
+    impl SilencerControlFlags : u8 {
+        const NONE              = 0;
+        const FIXED_UPDATE_RATE = 1 << 0;
+        const PULSE_WIDTH       = 1 << 1;
+        const STRICT_MODE       = 1 << 2;
+    }
+}
 
 pub use completion_steps::SilencerFixedCompletionStepsOp;
 pub use update_rate::SilencerFixedUpdateRateOp;
+use zerocopy::{Immutable, IntoBytes};
