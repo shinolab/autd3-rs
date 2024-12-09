32d989facf1065d6f16b33f72da65b72aa791d59
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-12-09 13:47:54 +0900

    add doc for `autd3-derive` crate

--- autd3-driver/src/firmware/cpu/datagram/tx.rs
+++ autd3-driver/src/firmware/cpu/datagram/tx.rs
@@ -12,6 +12,17 @@ const PAYLOAD_SIZE: usize = EC_OUTPUT_FRAME_SIZE - std::mem::size_of::<Header>()
 pub struct TxMessage {
     #[get(ref, ref_mut)]
     header: Header,
-    #[as_bytes]
     payload: [u16; PAYLOAD_SIZE / size_of::<u16>()], // use u16 for alignment
+}
+
+impl TxMessage {
+    #[must_use]
+    pub fn payload(&self) -> &[u8] {
+        self.payload.as_bytes()
+    }
+
+    #[must_use]
+    pub fn payload_mut(&mut self) -> &mut [u8] {
+        self.payload.as_mut_bytes()
+    }
 }
