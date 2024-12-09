f4eb2f707f3de8a2ace5c953820d4b27f794c194
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-12-09 09:47:46 +0900

    add doc for `autd3` crate

--- autd3/src/error.rs
+++ autd3/src/error.rs
@@ -1,13 +1,17 @@
 use autd3_driver::error::AUTDInternalError;
 use thiserror::Error;
 
+/// A interface for error handling in autd3.
 #[derive(Error, Debug, PartialEq)]
 #[non_exhaustive]
 pub enum AUTDError {
+    /// Failed to read firmware version.
     #[error("Read firmware info failed: {}", .0.iter().enumerate().filter(|(_, &b)| !b).map(|(i, _)| i.to_string()).collect::<Vec<_>>().join(", "))]
     ReadFirmwareVersionFailed(Vec<bool>),
+    /// Failed to read FPGA state.
     #[error("Read FPGA state failed")]
     ReadFPGAStateFailed,
+    /// Internal error.
     #[error("{0}")]
     Internal(#[from] AUTDInternalError),
 }
