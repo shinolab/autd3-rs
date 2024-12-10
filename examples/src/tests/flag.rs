9c8d077393d61e83511a936f99fbd95b212be8e4
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-12-10 12:44:50 +0900

    update example

--- examples/src/tests/flag.rs
+++ examples/src/tests/flag.rs
@@ -1,7 +1,3 @@
-use std::sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc,
-};
 use tokio::io::AsyncBufReadExt;
 
 use autd3::{driver::link::Link, prelude::*};
@@ -15,23 +11,23 @@ pub async fn flag(autd: &mut Controller<impl Link>) -> anyhow::Result<bool> {
 
     autd.send(ForceFan::new(|_dev| true)).await?;
 
-    let fin = Arc::new(AtomicBool::new(false));
+    let (tx, mut rx) = tokio::sync::oneshot::channel();
     println!("press any key to stop checking FPGA status...");
-    let fin_signal = tokio::spawn({
-        let fin = fin.clone();
-        async move {
-            let mut _s = String::new();
-            tokio::io::BufReader::new(tokio::io::stdin())
-                .read_line(&mut _s)
-                .await?;
-            fin.store(true, Ordering::Relaxed);
-            std::io::Result::Ok(())
-        }
+    let fin_signal = tokio::spawn(async move {
+        let mut _s = String::new();
+        tokio::io::BufReader::new(tokio::io::stdin())
+            .read_line(&mut _s)
+            .await?;
+        _ = tx.send(());
+        tokio::io::Result::Ok(())
     });
 
     let prompts = ['-', '/', '|', '\\'];
     let mut idx = 0;
-    while !fin.load(Ordering::Relaxed) {
+    loop {
+        if rx.try_recv().is_ok() {
+            break;
+        }
         let states = autd.fpga_state().await?;
         println!("{} FPGA Status...", prompts[idx / 1000 % prompts.len()]);
         idx += 1;
