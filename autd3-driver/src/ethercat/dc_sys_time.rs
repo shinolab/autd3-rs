4557f3f15715433145feec68cdb1d13084ddfdd6
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-12-19 11:29:01 +0900

    validate `DcSysTime::from_utc` method to check for u64 range

--- autd3-driver/src/ethercat/dc_sys_time.rs
+++ autd3-driver/src/ethercat/dc_sys_time.rs
@@ -22,12 +22,10 @@ impl DcSysTime {
     }
 
     pub fn from_utc(utc: OffsetDateTime) -> Result<Self, AUTDInternalError> {
-        match (utc - ECAT_DC_SYS_TIME_BASE).whole_nanoseconds() {
-            i if i < 0 => Err(AUTDInternalError::InvalidDateTime),
-            i => Ok(Self {
-                dc_sys_time: i as u64,
-            }),
-        }
+        Ok(Self {
+            dc_sys_time: u64::try_from((utc - ECAT_DC_SYS_TIME_BASE).whole_nanoseconds())
+                .map_err(|_| AUTDInternalError::InvalidDateTime)?,
+        })
     }
 
     pub fn now() -> Self {
@@ -71,6 +69,7 @@ mod tests {
     #[case(Ok(DcSysTime { dc_sys_time: 1000000000 }), time::macros::datetime!(2000-01-01 0:0:1 UTC))]
     #[case(Ok(DcSysTime { dc_sys_time: 31622400000000000 }), time::macros::datetime!(2001-01-01 0:0:0 UTC))]
     #[case(Err(AUTDInternalError::InvalidDateTime), time::macros::datetime!(1999-01-01 0:0:1 UTC))]
+    #[case(Err(AUTDInternalError::InvalidDateTime), time::macros::datetime!(9999-01-01 0:0:1 UTC))]
     fn from_utc(#[case] expect: Result<DcSysTime, AUTDInternalError>, #[case] utc: OffsetDateTime) {
         assert_eq!(expect, DcSysTime::from_utc(utc));
     }
