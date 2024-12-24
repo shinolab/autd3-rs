c8275d870dab1685ab4b4e7e1ef8bbd6740b08b0
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-12-24 12:36:16 +0900

    add doc to `autd3-link-twincat` crate

--- autd3-link-twincat/src/lib.rs
+++ autd3-link-twincat/src/lib.rs
@@ -1,11 +1,18 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(missing_docs)]
+#![warn(rustdoc::missing_crate_level_docs)]
+#![warn(rustdoc::unescaped_backticks)]
+
+//! This crate provides a link to AUTD using TwinCAT3.
 
 mod error;
 
 #[cfg(feature = "local")]
+/// Using TwinCAT3 on the local machine.
 pub mod local;
 #[cfg_attr(docsrs, doc(cfg(feature = "remote")))]
 #[cfg(feature = "remote")]
+/// Using TwinCAT3 on a remote machine.
 pub mod remote;
 
 #[cfg(feature = "local")]
