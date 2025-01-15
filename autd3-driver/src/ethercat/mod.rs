fc3208b72331232fb3cdcbe72ab3f12c0b7b614d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-01-15 17:11:49 +0900

    rm `SyncMode`

--- autd3-driver/src/ethercat/mod.rs
+++
@@ -1,7 +0,0 @@
-mod sync_mode;
-
-pub use autd3_core::ethercat::DcSysTime;
-pub use autd3_core::ethercat::{
-    ECAT_DC_SYS_TIME_BASE, EC_CYCLE_TIME_BASE, EC_INPUT_FRAME_SIZE, EC_OUTPUT_FRAME_SIZE,
-};
-pub use sync_mode::SyncMode;
