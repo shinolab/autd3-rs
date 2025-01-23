120244058e5923986093e81ed8f353766aced7e5
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-01-23 11:02:34 +0900

    adopt cargo-make

--- cross-aarch64.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-apt-get update
-apt-get install -y build-essential
-apt-get install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
--- cross-arm32.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-apt-get update
-apt-get install -y build-essential
-apt-get install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
