0f1a7ffef386c5e2620a7638d6398650485e2816
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-02-19 10:06:04 +0900

    fix `autd3-protobuf`

--- autd3-protobuf/src/traits/driver/firmware/fpga/sampling_config.rs
+++ autd3-protobuf/src/traits/driver/firmware/fpga/sampling_config.rs
@@ -3,6 +3,7 @@ use crate::{
     traits::{FromMessage, ToMessage},
     AUTDProtoBufError,
 };
+use std::num::NonZeroU16;
 
 impl ToMessage for autd3_driver::firmware::fpga::SamplingConfig {
     type Message = SamplingConfig;
@@ -12,14 +13,19 @@ impl ToMessage for autd3_driver::firmware::fpga::SamplingConfig {
         _: Option<&autd3_core::geometry::Geometry>,
     ) -> Result<Self::Message, AUTDProtoBufError> {
         Ok(Self::Message {
-            div: self.division.get() as _,
+            div: self
+                .division()
+                .map_err(autd3_driver::error::AUTDDriverError::from)? as _,
         })
     }
 }
 
 impl FromMessage<SamplingConfig> for autd3_driver::firmware::fpga::SamplingConfig {
     fn from_msg(msg: &SamplingConfig) -> Result<Self, AUTDProtoBufError> {
-        Self::new(u16::try_from(msg.div)?).map_err(|_| AUTDProtoBufError::DataParseError)
+        Ok(Self::new(
+            NonZeroU16::new(u16::try_from(msg.div).map_err(|_| AUTDProtoBufError::DataParseError)?)
+                .ok_or(AUTDProtoBufError::DataParseError)?,
+        ))
     }
 }
 
@@ -32,7 +38,7 @@ mod tests {
     #[test]
     fn test_sampling_config() {
         let mut rng = rand::rng();
-        let v = SamplingConfig::new(rng.random_range(0x0001..=0xFFFF)).unwrap();
+        let v = SamplingConfig::new(NonZeroU16::new(rng.random_range(0x0001..=0xFFFF)).unwrap());
         let msg = v.to_msg(None).unwrap();
         let v2 = SamplingConfig::from_msg(&msg).unwrap();
         assert_eq!(v, v2);
