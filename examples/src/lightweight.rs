a15e0abe257ecb62bad3fbc00852ae61eacb46fc
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-02-25 14:12:52 +0900

    impl `Deref<Target = Geometry>` for `LightweightClient`

--- examples/src/lightweight.rs
+++ examples/src/lightweight.rs
@@ -4,36 +4,30 @@ use autd3_protobuf::lightweight::LightweightClient;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut client = LightweightClient::open([AUTD3::default()], "127.0.0.1:8080".parse()?).await?;
+    let mut autd = LightweightClient::open([AUTD3::default()], "127.0.0.1:8080".parse()?).await?;
 
     println!("======== AUTD3 firmware information ========");
-    client
-        .firmware_version()
-        .await?
-        .iter()
-        .for_each(|firm_info| {
-            println!("{}", firm_info);
-        });
+    autd.firmware_version().await?.iter().for_each(|firm_info| {
+        println!("{}", firm_info);
+    });
     println!("============================================");
 
-    client
-        .send(Sine {
-            freq: 150. * Hz,
-            option: Default::default(),
-        })
-        .await?;
-    client
-        .send(Focus {
-            pos: Point3::new(90., 70., 150.),
-            option: Default::default(),
-        })
-        .await?;
+    autd.send(Sine {
+        freq: 150. * Hz,
+        option: Default::default(),
+    })
+    .await?;
+    autd.send(Focus {
+        pos: autd.center() + Vector3::new(0., 0., 150.),
+        option: Default::default(),
+    })
+    .await?;
 
     println!("Press enter to exit...");
     let mut _s = String::new();
     std::io::stdin().read_line(&mut _s)?;
 
-    client.close().await?;
+    autd.close().await?;
 
     Ok(())
 }
