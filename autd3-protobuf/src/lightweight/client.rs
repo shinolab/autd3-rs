d136f5924b9d662b11f97d4ffa78dbccb54bfe0b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-03-05 15:12:02 +0900

    add `sender` in `lightweight::Controller`

--- autd3-protobuf/src/lightweight/client.rs
+++ autd3-protobuf/src/lightweight/client.rs
@@ -4,7 +4,7 @@ use autd3_core::geometry::{Device, Geometry};
 
 use derive_more::Deref;
 
-use crate::{OpenRequestLightweight, traits::*};
+use crate::{OpenRequestLightweight, SenderOption, traits::*};
 
 type Client = crate::pb::ecat_light_client::EcatLightClient<tonic::transport::Channel>;
 
@@ -15,10 +15,19 @@ pub struct Controller {
     geometry: Geometry,
 }
 
+pub struct Sender<'a, S: autd3::r#async::controller::AsyncSleep>
+where
+    autd3::controller::SenderOption<S>: ToMessage<Message = SenderOption>,
+{
+    pub(crate) controller: &'a mut Controller,
+    pub(crate) option: autd3::controller::SenderOption<S>,
+}
+
 pub trait Datagram {
     fn send(
         self,
         client: &mut Client,
+        sender_option: Option<crate::SenderOption>,
     ) -> impl std::future::Future<Output = Result<(), crate::error::AUTDProtoBufError>>;
 }
 
@@ -26,11 +35,18 @@ impl<T> Datagram for T
 where
     T: ToMessage<Message = crate::pb::Datagram>,
 {
-    async fn send(self, client: &mut Client) -> Result<(), crate::error::AUTDProtoBufError> {
+    async fn send(
+        self,
+        client: &mut Client,
+        sender_option: Option<crate::SenderOption>,
+    ) -> Result<(), crate::error::AUTDProtoBufError> {
         let res = client
-            .send(tonic::Request::new(crate::DatagramTuple {
-                first: Some(self.to_msg(None)?),
-                second: None,
+            .send(tonic::Request::new(crate::SendRequestLightweight {
+                datagram: Some(crate::DatagramTuple {
+                    first: Some(self.to_msg(None)?),
+                    second: None,
+                }),
+                sender_option,
             }))
             .await?
             .into_inner();
@@ -46,12 +62,19 @@ where
     T1: ToMessage<Message = crate::pb::Datagram>,
     T2: ToMessage<Message = crate::pb::Datagram>,
 {
-    async fn send(self, client: &mut Client) -> Result<(), crate::error::AUTDProtoBufError> {
+    async fn send(
+        self,
+        client: &mut Client,
+        sender_option: Option<crate::SenderOption>,
+    ) -> Result<(), crate::error::AUTDProtoBufError> {
         let (d1, d2) = self;
         let res = client
-            .send(tonic::Request::new(crate::DatagramTuple {
-                first: Some(d1.to_msg(None)?),
-                second: Some(d2.to_msg(None)?),
+            .send(tonic::Request::new(crate::SendRequestLightweight {
+                datagram: Some(crate::DatagramTuple {
+                    first: Some(d1.to_msg(None)?),
+                    second: Some(d2.to_msg(None)?),
+                }),
+                sender_option,
             }))
             .await?
             .into_inner();
@@ -67,12 +90,33 @@ impl Controller {
         devices: F,
         addr: SocketAddr,
     ) -> Result<Self, crate::error::AUTDProtoBufError> {
-        Controller::open_impl(devices.into_iter().map(|d| d.into()).collect(), addr).await
+        Controller::open_impl(devices.into_iter().map(|d| d.into()).collect(), addr, None).await
+    }
+
+    pub async fn open_with_option<
+        D: Into<Device>,
+        F: IntoIterator<Item = D>,
+        S: autd3::r#async::controller::AsyncSleep,
+    >(
+        devices: F,
+        addr: SocketAddr,
+        sender_option: autd3::controller::SenderOption<S>,
+    ) -> Result<Self, crate::error::AUTDProtoBufError>
+    where
+        autd3::controller::SenderOption<S>: ToMessage<Message = SenderOption>,
+    {
+        Controller::open_impl(
+            devices.into_iter().map(|d| d.into()).collect(),
+            addr,
+            Some(sender_option.to_msg(None)?),
+        )
+        .await
     }
 
     async fn open_impl(
         devices: Vec<Device>,
         addr: SocketAddr,
+        sender_option: Option<crate::SenderOption>,
     ) -> Result<Self, crate::error::AUTDProtoBufError> {
         let conn = tonic::transport::Endpoint::new(format!("http://{}", addr))?
             .connect()
@@ -82,6 +126,7 @@ impl Controller {
         let res = client
             .open(OpenRequestLightweight {
                 geometry: Some(geometry.to_msg(None)?),
+                sender_option,
             })
             .await?
             .into_inner();
@@ -131,7 +176,7 @@ impl Controller {
         &mut self,
         datagram: impl Datagram,
     ) -> Result<(), crate::error::AUTDProtoBufError> {
-        datagram.send(&mut self.client).await
+        datagram.send(&mut self.client, None).await
     }
 
     pub async fn close(mut self) -> Result<(), crate::error::AUTDProtoBufError> {
@@ -144,5 +189,32 @@ impl Controller {
             return Err(crate::error::AUTDProtoBufError::SendError(res.msg));
         }
         Ok(())
+    }
+
+    pub fn sender<S: autd3::r#async::controller::AsyncSleep>(
+        &mut self,
+        option: autd3::controller::SenderOption<S>,
+    ) -> Sender<S>
+    where
+        autd3::controller::SenderOption<S>: ToMessage<Message = SenderOption>,
+    {
+        Sender {
+            controller: self,
+            option,
+        }
+    }
+}
+
+impl<S: autd3::r#async::controller::AsyncSleep> Sender<'_, S>
+where
+    autd3::controller::SenderOption<S>: ToMessage<Message = SenderOption>,
+{
+    pub async fn send(
+        &mut self,
+        datagram: impl Datagram,
+    ) -> Result<(), crate::error::AUTDProtoBufError> {
+        datagram
+            .send(&mut self.controller.client, Some(self.option.to_msg(None)?))
+            .await
     }
 }
