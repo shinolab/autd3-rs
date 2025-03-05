67aa9b5292cae346fbebdb49e1ab9039f26fdf6f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-03-05 16:54:36 +0900

    fix `FociSTM` in lightweight mode

--- autd3-protobuf/src/traits/driver/datagram/stm/foci.rs
+++ autd3-protobuf/src/traits/driver/datagram/stm/foci.rs
@@ -5,18 +5,20 @@ use crate::{pb::*, traits::*};
 impl<const N: usize, C: Into<STMConfig> + Copy> ToMessage
     for autd3_driver::datagram::FociSTM<N, Vec<autd3_driver::datagram::ControlPoints<N>>, C>
 {
-    type Message = FociStm;
+    type Message = Datagram;
 
     fn to_msg(
         &self,
         _: Option<&autd3_core::geometry::Geometry>,
     ) -> Result<Self::Message, AUTDProtoBufError> {
         Ok(Self::Message {
-            foci: self
-                .iter()
-                .map(|p| p.to_msg(None))
-                .collect::<Result<Vec<_>, AUTDProtoBufError>>()?,
-            sampling_config: Some(self.sampling_config()?.to_msg(None)?),
+            datagram: Some(datagram::Datagram::FociStm(FociStm {
+                foci: self
+                    .iter()
+                    .map(|p| p.to_msg(None))
+                    .collect::<Result<Vec<_>, AUTDProtoBufError>>()?,
+                sampling_config: Some(self.sampling_config()?.to_msg(None)?),
+            })),
         })
     }
 }
