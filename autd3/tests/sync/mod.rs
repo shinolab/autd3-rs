5838b24c6f2084df252f1e318ac45ed73e13a60b
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-05-09 13:25:09 +0900

    fix: No need to check the response of disabled devices (#247)

--- autd3/tests/sync/mod.rs
+++ autd3/tests/sync/mod.rs
@@ -1,7 +1,8 @@
 use autd3::{
     Controller,
+    gain::Null,
     link::{Audit, AuditOption},
-    prelude::AUTD3,
+    prelude::{AUTD3, AUTDDriverError},
 };
 
 mod datagram;
@@ -25,6 +26,32 @@ fn initial_msg_id() -> anyhow::Result<()> {
             .iter()
             .all(|v| v.0 == 0x00)
     );
+
+    Ok(())
+}
+
+#[test]
+fn test_retry_with_disabled_device() -> anyhow::Result<()> {
+    let mut cnt = Controller::open([AUTD3::default(); 2], Audit::new(Default::default()))?;
+
+    assert_eq!(Ok(()), cnt.send(Null {}));
+
+    cnt.link_mut()[0].break_down();
+    assert_eq!(
+        Err(AUTDDriverError::ConfirmResponseFailed),
+        cnt.send(Null {})
+    );
+
+    cnt.link_mut()[0].repair();
+    assert_eq!(Ok(()), cnt.send(Null {}));
+
+    cnt.link_mut()[0].break_down();
+    assert_eq!(
+        Err(AUTDDriverError::ConfirmResponseFailed),
+        cnt.send(Null {})
+    );
+    cnt[0].enable = false;
+    assert_eq!(Ok(()), cnt.send(Null {}));
 
     Ok(())
 }
