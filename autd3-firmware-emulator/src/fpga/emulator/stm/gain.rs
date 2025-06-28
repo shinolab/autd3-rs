f02d14e5fd265347d27936310a010c7b416c1660
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-06-28 10:31:20 +0900

    perf: pass buffer to avoid allocation in firmware-emulator

--- autd3-firmware-emulator/src/fpga/emulator/stm/gain.rs
+++ autd3-firmware-emulator/src/fpga/emulator/stm/gain.rs
@@ -6,16 +6,26 @@ use autd3_core::{
 use crate::FPGAEmulator;
 
 impl FPGAEmulator {
-    pub(crate) fn gain_stm_drives_inplace(&self, segment: Segment, idx: usize, dst: &mut [Drive]) {
+    pub(crate) fn gain_stm_drives_inplace(
+        &self,
+        segment: Segment,
+        idx: usize,
+        phase_corr_buf: &mut [Phase],
+        output_mask_buf: &mut [bool],
+        dst: &mut [Drive],
+    ) {
+        self.phase_correction_inplace(phase_corr_buf);
+        self.output_mask_inplace(segment, output_mask_buf);
+
         self.mem.stm_bram[&segment]
             .mem()
             .iter()
             .skip(256 * idx)
-            .zip(self.phase_correction())
-            .zip(self.output_mask(segment))
+            .zip(phase_corr_buf.iter())
+            .zip(output_mask_buf.iter())
             .take(self.mem.num_transducers)
             .enumerate()
-            .for_each(|(i, ((&d, p), mask))| {
+            .for_each(|(i, ((&d, &p), &mask))| {
                 dst[i] = Drive {
                     phase: Phase((d & 0xFF) as u8) + p,
                     intensity: Intensity(if mask { ((d >> 8) & 0xFF) as u8 } else { 0x00 }),
