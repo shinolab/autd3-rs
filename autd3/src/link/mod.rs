cba51519a409ff59116992215889f408f793eb6b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-06-30 11:11:47 +0900

    feat!: make `link::Audit` and `link::Nop` optional

--- autd3/src/link/mod.rs
+++ autd3/src/link/mod.rs
@@ -1,6 +1,12 @@
+#[cfg_attr(docsrs, doc(cfg(feature = "link-audit")))]
+#[cfg(feature = "link-audit")]
 #[doc(hidden)]
 pub mod audit;
+#[cfg_attr(docsrs, doc(cfg(feature = "link-nop")))]
+#[cfg(feature = "link-nop")]
 mod nop;
 
+#[cfg(feature = "link-audit")]
 pub use audit::{Audit, AuditOption};
+#[cfg(feature = "link-nop")]
 pub use nop::Nop;
