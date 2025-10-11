a53da5b068a99598ee2b3a313ee2f55b9e524e2e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-10-11 12:08:17 +0900

    remove!: `RemoteTwinCAT` link

--- autd3-link-twincat/src/lib.rs
+++ autd3-link-twincat/src/lib.rs
@@ -7,17 +7,12 @@
 
 mod error;
 
-#[cfg(feature = "local")]
-/// Using TwinCAT3 on the local machine.
-pub mod local;
-#[cfg_attr(docsrs, doc(cfg(feature = "remote")))]
-#[cfg(feature = "remote")]
-/// Using TwinCAT3 on a remote machine.
-pub mod remote;
+#[cfg(target_os = "windows")]
+mod win;
+#[cfg(target_os = "windows")]
+pub use win::*;
 
-#[cfg(feature = "local")]
-pub use local::TwinCAT;
-
-#[cfg_attr(docsrs, doc(cfg(feature = "remote")))]
-#[cfg(feature = "remote")]
-pub use remote::{RemoteTwinCAT, RemoteTwinCATOption};
+#[cfg(not(target_os = "windows"))]
+mod unix;
+#[cfg(not(target_os = "windows"))]
+pub use unix::*;
--- autd3-link-twincat/src/remote/mod.rs
+++
@@ -1,4 +0,0 @@
-mod native_methods;
-mod remote_twincat_link;
-
-pub use remote_twincat_link::{RemoteTwinCAT, RemoteTwinCATOption};
