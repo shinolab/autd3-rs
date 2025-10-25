b7861ed432e490f675325fdec0703587cc17877b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-10-25 09:24:46 +0900

    fix: update tracing target

--- examples/src/ethercrab.rs
+++ examples/src/ethercrab.rs
@@ -5,7 +5,7 @@ use autd3_link_ethercrab::{EtherCrab, EtherCrabOption};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
-        .with_env_filter("autd3-link-ethercrab=info")
+        .with_env_filter("autd3_link_ethercrab=info")
         .init();
 
     let autd = Controller::open(
