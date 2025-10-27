b31e62595e5525af2e421950ebb57948c7d9af93
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-10-27 10:37:31 +0900

    tidy up sleep

--- autd3-core/src/sleep/mod.rs
+++ autd3-core/src/sleep/mod.rs
@@ -8,17 +8,15 @@ unsafe extern "system" {
 
 /// A trait for sleep operations.
 pub trait Sleeper {
-    /// Sleep until the specified deadline.
+    /// Sleep for the specified duration.
     fn sleep(&self, duration: Duration);
 }
 
-// GRCOV_EXCL_START
 impl Sleeper for Box<dyn Sleeper> {
     fn sleep(&self, duration: Duration) {
         self.as_ref().sleep(duration);
     }
 }
-// GRCOV_EXCL_STOP
 
 /// A sleeper that uses [`std::thread::sleep`].
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
@@ -27,7 +25,7 @@ pub struct StdSleeper;
 impl Sleeper for StdSleeper {
     fn sleep(&self, duration: Duration) {
         if duration.is_zero() {
-            return; // GRCOV_EXCL_LINE
+            return;
         }
 
         #[cfg(target_os = "windows")]
@@ -47,7 +45,6 @@ impl Sleeper for StdSleeper {
 pub struct SpinWaitSleeper;
 
 impl Sleeper for SpinWaitSleeper {
-    // GRCOV_EXCL_START
     fn sleep(&self, duration: Duration) {
         use std::time::Instant;
 
@@ -56,5 +53,54 @@ impl Sleeper for SpinWaitSleeper {
             core::hint::spin_loop();
         }
     }
-    // GRCOV_EXCL_STOP
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn std_sleeper() {
+        let sleeper = StdSleeper;
+        {
+            let start = std::time::Instant::now();
+            sleeper.sleep(Duration::from_millis(10));
+            assert!(Duration::from_millis(10) <= start.elapsed());
+        }
+        {
+            let start = std::time::Instant::now();
+            sleeper.sleep(Duration::from_millis(0));
+            assert!(Duration::from_millis(0) <= start.elapsed());
+        }
+    }
+
+    #[test]
+    fn spin_wait_sleeper() {
+        let sleeper = SpinWaitSleeper;
+        {
+            let start = std::time::Instant::now();
+            sleeper.sleep(Duration::from_millis(10));
+            assert!(Duration::from_millis(10) <= start.elapsed());
+        }
+        {
+            let start = std::time::Instant::now();
+            sleeper.sleep(Duration::from_millis(0));
+            assert!(Duration::from_millis(0) <= start.elapsed());
+        }
+    }
+
+    #[test]
+    fn box_sleeper() {
+        let sleeper: Box<dyn Sleeper> = Box::new(StdSleeper);
+        {
+            let start = std::time::Instant::now();
+            sleeper.sleep(Duration::from_millis(10));
+            assert!(Duration::from_millis(10) <= start.elapsed());
+        }
+        {
+            let start = std::time::Instant::now();
+            sleeper.sleep(Duration::from_millis(0));
+            assert!(Duration::from_millis(0) <= start.elapsed());
+        }
+    }
 }
