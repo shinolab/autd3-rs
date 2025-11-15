38e0bafe5e2d9f89ee1db3134bdedf364af7f342
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-11-15 12:33:58 +0900

    rm unused error variant

--- autd3-link-twincat/src/error.rs
+++ autd3-link-twincat/src/error.rs
@@ -6,14 +6,9 @@ pub enum AdsError {
     DllNotFound,
     FunctionNotFound(String),
     OpenPort,
-    ClosePort,
-    DeviceInvalidSize,
     GetLocalAddress(i32),
-    AmsNetIdParse,
-    AmsAddRoute(i32),
     SendData(i32),
     ReadData(i32),
-    InvalidIp(String),
     #[cfg(feature = "remote")]
     Ads(ads::Error),
 }
@@ -26,14 +21,9 @@ impl std::fmt::Display for AdsError {
                 write!(f, "Function {} not found in TcAdsDll", name)
             }
             AdsError::OpenPort => write!(f, "Failed to open port"),
-            AdsError::ClosePort => write!(f, "Failed to close port"),
-            AdsError::DeviceInvalidSize => write!(f, "The number of devices is invalid"),
             AdsError::GetLocalAddress(code) => write!(f, "Failed to get local address: {}", code),
-            AdsError::AmsNetIdParse => write!(f, "Ams net id must have 6 octets"),
-            AdsError::AmsAddRoute(code) => write!(f, "Failed to add route: {}", code),
             AdsError::SendData(code) => write!(f, "Failed to send data: {}", code),
             AdsError::ReadData(code) => write!(f, "Failed to read data: {}", code),
-            AdsError::InvalidIp(ip) => write!(f, "Invalid IP address: {}", ip),
             #[cfg(feature = "remote")]
             AdsError::Ads(err) => write!(f, "{}", err),
         }
